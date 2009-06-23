bedf8fab22a9ee497433d6a4abe1ca283a38e8cb
julien <julien@redweb.dk>
2009-06-23 14:36:09 +0000

    details column for event_venue_xref was missing in install sql

--- component/admin/install.mysql.utf8.sql
+++ component/admin/install.mysql.utf8.sql
@@ -229,6 +229,7 @@ CREATE TABLE IF NOT EXISTS `#__redevent_event_venue_xref` (
   `enddates` date NULL default NULL,
   `times` time NULL default NULL,
   `endtimes` time NULL default NULL,
+  `details` text NOT NULL,
   `maxattendees` int(11) NOT NULL default '0',
   `maxwaitinglist` int(11) NOT NULL default '0',
   `course_credit` int(11) NOT NULL,
