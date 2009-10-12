a8c602a139698291b70281a5fd45882956fd2912
julien <julien@redweb.dk>
2009-10-12 13:29:44 +0000

    added button to reset categories element.

--- component/admin/elements/categories.php
+++ component/admin/elements/categories.php
@@ -71,6 +71,7 @@ class JElementCategories extends JElement
 
 		$html = "\n<div style=\"float: left;\"><input style=\"background: #ffffff;\" type=\"text\" id=\"a_name\" value=\"$category->catname\" disabled=\"disabled\" /></div>";
 		$html .= "<div class=\"button2-left\"><div class=\"blank\"><a class=\"modal\" title=\"".JText::_('Select')."\"  href=\"$link\" rel=\"{handler: 'iframe', size: {x: 650, y: 375}}\">".JText::_('Select')."</a></div></div>\n";
+    $html .= "<div class=\"button2-left\"><div class=\"blank\"><a title=\"".JText::_('Reset')."\"  href=\"#\" onClick=\"elSelectCategory('', '".JText::_('SELECT CATEGORY')."');\">".JText::_('Reset')."</a></div></div>\n";
 		$html .= "\n<input type=\"hidden\" id=\"a_id\" name=\"$fieldName\" value=\"$value\" />";
 
 		return $html;
