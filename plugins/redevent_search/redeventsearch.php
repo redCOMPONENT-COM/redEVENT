cd0fc33020d81528ca82d0cd0d29e0917ff48772
julien <julien@redweb.dk>
2009-10-18 16:45:00 +0000

    added redevent search plugin.

---
+++ plugins/redevent_search/redeventsearch.php
@@ -0,0 +1,303 @@
+<?php
+/**
+ * @version 1.0 $Id: admin.class.php 662 2008-05-09 22:28:53Z schlu $
+ * @package Joomla
+ * @subpackage redEVENT
+ * @copyright redEVENT (C) 2008 redCOMPONENT.com / EventList (C) 2005 - 2008 Christoph Lukes
+ * @license GNU/GPL, see LICENSE.php
+ * redEVENT is based on EventList made by Christoph Lukes from schlu.net
+ * redEVENT can be downloaded from www.redcomponent.com
+ * redEVENT is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License 2
+ * as published by the Free Software Foundation.
+
+ * redEVENT is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with redEVENT; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+// no direct access
+defined( '_JEXEC' ) or die( 'Restricted access' );
+ 
+//define the registerEvent and the language file. 
+$mainframe->registerEvent( 'onSearch', 'plgSearchRedeventSearch' );
+$mainframe->registerEvent( 'onSearchAreas', 'plgSearchRedeventSearchAreas' );
+ 
+JPlugin::loadLanguage( 'plg_search_redeventsearch', 'administrator' );
+ 
+//define a function to return an array of search areas.
+function &plgSearchRedeventSearchAreas()
+{
+        static $areas = array(
+                'redeventevents' => 'REDEVENT EVENTS',
+                'redeventcategories' => 'REDEVENT CATEGORIES',
+                'redeventvenues' => 'REDEVENT VENUES',
+        );
+        return $areas;
+}
+ 
+//Then the real function has to be created. The database connection should be made. 
+function plgSearchRedeventSearch( $text, $phrase='', $ordering='', $areas=null )
+{
+	$db            =& JFactory::getDBO();
+	$user  =& JFactory::getUser();
+
+	//If the array is not correct, return it:
+	if (is_array( $areas )) {
+		if (!array_intersect( $areas, array_keys( plgSearchRedeventSearchAreas() ) )) {
+			return array();
+		}
+	}
+
+	//It is time to define the parameters! First get the right plugin; 'search' (the group), 'nameofplugin'.
+	$plugin =& JPluginHelper::getPlugin('search', 'redeventsearch');
+
+	//Then load the parameters of the plugin..
+	$pluginParams = new JParameter( $plugin->params );
+	
+  $limit = $pluginParams->def( 'search_limit', 50 );
+
+	//Use the function trim to delete spaces in front of or at the back of the searching terms
+	$text = trim( $text );
+
+	//Return Array when nothing was filled in
+	if ($text == '') {
+		return array();
+	}
+
+	$rows = array();
+	
+	$search = $db->Quote(JText::_( 'EVENTS' ));
+
+	if (!$areas || in_array('redeventevents', $areas))
+	{
+		$where = array();
+		$where[] = 'x.published = 1';
+		switch ($phrase) {
+
+			//search exact
+			case 'exact':
+				$string        = $db->Quote( '%'.$db->getEscaped( $text, true ).'%', false );
+				$where[]   = 'LOWER(e.title) LIKE '.$string;
+				break;
+
+				//search all or any
+			case 'all':
+			case 'any':
+
+				//set default
+			default:
+				$words         = explode( ' ', $text );
+				$wheres = array();
+				foreach ($words as $word)
+				{
+					$word          = $db->Quote( '%'.$db->getEscaped( $word, true ).'%', false );
+	        $wheres[]   = 'LOWER(e.title) LIKE '.$word;
+				}
+				$where[] = '(' . implode( ($phrase == 'all' ? ') AND (' : ') OR ('), $wheres ) . ')';
+				break;
+		}
+
+		//ordering of the results
+		switch ( $ordering ) {
+
+			//alphabetic, ascending
+			case 'alpha':
+				$order = 'e.title ASC, x.dates ASC';
+				break;
+
+				//oldest first
+			case 'oldest':
+        $order = 'x.dates ASC';
+
+				//popular first
+			case 'popular':
+
+				//newest first
+			case 'newest':
+        $order = 'x.dates DESC';
+
+				//default setting: alphabetic, ascending
+			default:
+        $order = 'x.dates ASC';
+		}
+
+		//the database query; 
+		$query = 'SELECT title, x.id AS xref, '
+		. ' CONCAT_WS( " / ", '. $search .', '.$db->Quote(JText::_( 'EVENTS' )).' ) AS section,'
+    . ' CASE WHEN CHAR_LENGTH( e.alias ) THEN CONCAT_WS( \':\', x.id, e.alias ) ELSE x.id END AS slug, '
+    . ' NULL AS created, '
+		. ' "2" AS browsernav'
+		. ' FROM #__redevent_events AS e'
+    . ' INNER JOIN #__redevent_event_venue_xref AS x ON x.eventid = e.id '
+		. ' WHERE ( '. implode(' AND ', $where) .' )'
+		. ' ORDER BY '. $order
+		;
+
+		//Set query
+		$db->setQuery( $query, 0, $limit );
+		$results = $db->loadObjectList();
+
+		//The 'output' of the displayed link
+		foreach($results as $key => $row) {
+			$results[$key]->href = 'index.php?option=com_redevent&view=details&id='.$row->slug.'&xref='.$row->xref;
+		}
+		
+		$rows = array_merge($rows, $results);
+	}
+	
+ if (!$areas || in_array('redeventcategories', $areas))
+  {
+    $where = array();
+    $where[] = 'c.published = 1';
+    switch ($phrase) {
+
+      //search exact
+      case 'exact':
+        $string          = $db->Quote( '%'.$db->getEscaped( $text, true ).'%', false );
+        $where[]   = 'LOWER(c.catname) LIKE '.$string;
+        break;
+
+        //search all or any
+      case 'all':
+      case 'any':
+
+        //set default
+      default:
+        $words         = explode( ' ', $text );
+        $wheres        = array();
+        foreach ($words as $word)
+        {
+          $word          = $db->Quote( '%'.$db->getEscaped( $word, true ).'%', false );
+          $wheres[]      = 'LOWER(c.catname) LIKE '.$word;
+        }
+        $where[] = '(' . implode( ($phrase == 'all' ? ') AND (' : ') OR ('), $wheres ) . ')';
+        break;
+    }
+
+    //ordering of the results
+    switch ( $ordering ) {
+
+      //alphabetic, ascending
+      case 'alpha':
+        $order = 'c.catname ASC';
+        break;
+
+        //oldest first
+      case 'oldest':
+
+        //popular first
+      case 'popular':
+
+        //newest first
+      case 'newest':
+
+        //default setting: alphabetic, ascending
+      default:
+        $order = 'c.catname ASC';
+    }
+
+    //the database query; differs per situation! It will look something like this:
+    $query = 'SELECT c.catname AS title,'
+    . ' CONCAT_WS( " / ", '. $search .', '.$db->Quote(JText::_( 'CATEGORIES' )).' ) AS section,'
+    . ' CASE WHEN CHAR_LENGTH( c.alias ) THEN CONCAT_WS( \':\', c.id, c.alias ) ELSE c.id END AS slug, '
+    . ' NULL AS created, '
+    . ' "2" AS browsernav'
+    . ' FROM #__redevent_categories AS c'
+    . ' WHERE ( '. implode(' AND ', $where) .' )'
+    . ' ORDER BY '. $order
+    ;
+
+    //Set query
+    $db->setQuery( $query, 0, $limit );
+    $results = $db->loadObjectList();
+
+    //The 'output' of the displayed link
+    foreach($results as $key => $row) {
+      $results[$key]->href = 'index.php?option=com_redevent&view=categoryevents&id='.$row->slug;
+    }
+    $rows = array_merge($rows, $results);
+  }
+
+  if (!$areas || in_array('redeventvenues', $areas))
+  {
+    $where = array();
+    switch ($phrase) {
+
+      //search exact
+      case 'exact':
+        $string          = $db->Quote( '%'.$db->getEscaped( $text, true ).'%', false );
+        $where[]   = 'LOWER(v.venue) LIKE '.$string;
+        break;
+
+        //search all or any
+      case 'all':
+      case 'any':
+
+        //set default
+      default:
+        $words         = explode( ' ', $text );
+        $wheres = array();
+        foreach ($words as $word)
+        {
+          $word          = $db->Quote( '%'.$db->getEscaped( $word, true ).'%', false );
+          $wheres[]   = 'LOWER(v.venue) LIKE '.$word;
+        }
+        $where[] = '(' . implode( ($phrase == 'all' ? ') AND (' : ') OR ('), $wheres ) . ')';
+        break;
+    }
+
+    //ordering of the results
+    switch ( $ordering ) {
+
+      //alphabetic, ascending
+      case 'alpha':
+        $order = 'v.venue ASC';
+        break;
+
+        //oldest first
+      case 'oldest':
+
+        //popular first
+      case 'popular':
+
+        //newest first
+      case 'newest':
+
+        //default setting: alphabetic, ascending
+      default:
+        $order = 'v.venue ASC';
+    }
+
+    //the database query; differs per situation! It will look something like this:
+    $query = 'SELECT v.venue AS title,'
+    . ' CONCAT_WS( " / ", '. $search .', '.$db->Quote(JText::_( 'VENUES' )).' ) AS section,'
+    . ' CASE WHEN CHAR_LENGTH( v.alias ) THEN CONCAT_WS( \':\', v.id, v.alias ) ELSE v.id END AS slug, '
+    . ' NULL AS created, '
+    . ' "2" AS browsernav'
+    . ' FROM #__redevent_venues AS v'
+    . ' WHERE ( '. implode(' AND ', $where) .' )'
+    . ' ORDER BY '. $order
+    ;
+
+    //Set query
+    $db->setQuery( $query, 0, $limit );
+    $results = $db->loadObjectList();
+
+    //The 'output' of the displayed link
+    foreach($results as $key => $row) {
+      $results[$key]->href = 'index.php?option=com_redevent&view=venueevents&id='.$row->slug;
+    }
+    $rows = array_merge($rows, $results);
+  }
+  
+	//Return the search results in an array
+	return $rows;
+}
+
+?>
