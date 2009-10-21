fd44ecdeeb64c1c9c7b16599f4a34bce9e65f4dd
julien <julien@redweb.dk>
2009-10-21 12:28:27 +0000

    fixed submenu for cleanup view.

--- component/admin/views/cleanup/view.html.php
+++ component/admin/views/cleanup/view.html.php
@@ -51,17 +51,8 @@ class RedEventViewCleanup extends JView {
 		//add css and submenu to document
 		$document->addStyleSheet('components/com_redevent/assets/css/redeventbackend.css');
 
-		//Create Submenu
-		JSubMenuHelper::addEntry( JText::_( 'REDEVENT' ), 'index.php?option=com_redevent');
-		JSubMenuHelper::addEntry( JText::_( 'EVENTS' ), 'index.php?option=com_redevent&view=events');
-		JSubMenuHelper::addEntry( JText::_( 'VENUES' ), 'index.php?option=com_redevent&view=venues');
-		JSubMenuHelper::addEntry( JText::_( 'CATEGORIES' ), 'index.php?option=com_redevent&view=categories');
-		JSubMenuHelper::addEntry( JText::_( 'ARCHIVESCREEN' ), 'index.php?option=com_redevent&view=archive');
-		JSubMenuHelper::addEntry( JText::_( 'GROUPS' ), 'index.php?option=com_redevent&view=groups');
-		JSubMenuHelper::addEntry( JText::_( 'HELP' ), 'index.php?option=com_redevent&view=help');
-		if ($user->get('gid') > 24) {
-			JSubMenuHelper::addEntry( JText::_( 'SETTINGS' ), 'index.php?option=com_redevent&controller=settings&task=edit');
-		}
+    //Create Submenu
+    ELAdmin::setMenu();
 
 		//create the toolbar
 		JToolBarHelper::title( JText::_( 'CLEANUP' ), 'housekeeping' );
