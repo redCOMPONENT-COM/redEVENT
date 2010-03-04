c0bf9aa1b77a84fe4f757bab9be3dbd0a2bf2310
julien <julien@redweb.dk>
2010-03-04 10:12:41 +0000

    acl update

--- component/site/models/details.php
+++ component/site/models/details.php
@@ -567,17 +567,8 @@ class RedeventModelDetails extends JModel
   	if ($user->authorize('com_redevent', 'manageattendees')) {
   		return true;
   	}
-  	$query = ' SELECT gm.id '
-  	       . ' FROM #__redevent_event_venue_xref AS x '
-  	       . ' INNER JOIN #__redevent_groups AS g ON x.groupid = g.id '
-  	       . ' INNER JOIN #__redevent_groupmembers AS gm ON gm.group_id = g.id '
-  	       . ' WHERE gm.member = '. $this->_db->Quote($user->get('id'))
-  	       . '   AND (gm.manage_xrefs > 0 OR gm.manage_events > 0) '
-  	       . '   AND x.id = '. $this->_db->Quote($this->_xref)
-  	       ;
-  	$this->_db->setQuery($query);
-  	$res = $this->_db->loadObjectList();
-  	return (count($res) > 0);
+  	$acl = UserAcl::getInstance();
+  	return $acl->canEditXref($this->_xref);
   }
 }
 ?>
--- component/site/models/myevents.php
+++ component/site/models/myevents.php
@@ -479,7 +479,7 @@ class RedeventModelMyevents extends RedeventModelBaseEventList
         // then if the user is the owner of the event or member of admin group
         $where .= ' AND (a.created_by = '. $this->_db->Quote($user->id)
                 . '      OR (gm.member = '. $this->_db->Quote($user->id) 
-                . '          AND (gm.manage_events > 0 OR (gm.manage_xrefs > 0 AND gv.accesslevel > 0)) AND gc.accesslevel > 0)) ';
+                . '          AND (gm.manage_events > 0 OR gm.manage_xrefs > 0) AND gv.accesslevel > 0 AND gc.accesslevel > 0)) ';
 
         // Second is to only select events assigned to category the user has access to
         $where .= ' AND c.access <= '.$gid;
