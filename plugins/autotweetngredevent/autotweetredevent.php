ae5c0b9850d687dfc6291768003065f73461ddfd
johnny <johnny@redweb.dk>
2010-03-26 10:14:26 +0000

    copyright notice

--- plugins/autotweetngredevent/autotweetredevent.php
+++ plugins/autotweetngredevent/autotweetredevent.php
@@ -1,8 +1,21 @@
 <?php
-/**
- * @version	1.0
- * @author	Redweb.dk
- * @license	GPL 2.0
+/** 
+ * @copyright Copyright (C) 2010 redCOMPONENT.com. All rights reserved. 
+ * @license GNU/GPL, see LICENSE.php
+ * autotweetredevent can be downloaded from www.redcomponent.com
+ * autotweetredevent is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License 2
+ * as published by the Free Software Foundation.
+
+ * autotweetredevent is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with autotweetredevent; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
  */
 
 // Check to ensure this file is included in Joomla!
