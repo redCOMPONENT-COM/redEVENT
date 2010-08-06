85811b2c83bfb4d61fc8ca6314d9e96b9f3f0c5d
julien <julien@redweb.dk>
2010-08-06 11:06:05 +0200

    / fixed js error after filtering in event and xref elements

--- component/admin/views/xrefelement/tmpl/default.php
+++ component/admin/views/xrefelement/tmpl/default.php
@@ -24,7 +24,7 @@
 defined('_JEXEC') or die('Restricted access');
 ?>
 
-<form action="index.php?option=com_redevent&amp;view=xrefelement&amp;tmpl=component" method="post" name="adminForm">
+<form action="index.php?option=com_redevent&amp;view=xrefelement&amp;tmpl=component&field=<?php echo $this->field; ?>" method="post" name="adminForm">
 
 <table class="adminform">
 	<tr>
