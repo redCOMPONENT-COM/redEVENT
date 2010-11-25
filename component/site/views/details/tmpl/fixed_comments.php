d3f279a5874077966329b76fdf0cb3083cab3192
julien <julien@redweb.dk>
2010-11-25 09:12:53 +0100

    / added missing layout fixed_comments

---
+++ component/site/views/details/tmpl/fixed_comments.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * @version 1.0 $Id$
+ * @package Joomla
+ * @subpackage redEVENT
+ * @copyright redEVENT (C) 2008 redCOMPONENT.com / EventList (C) 2005 - 2008 Christoph Lukes
+ * @license GNU/GPL, see LICENSE.php
+ * redEVENT is based on EventList made by Christoph Lukes from schlu.net
+ * redEVENT can be downloaded from www.redcomponent.com
+ * redEVENT is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License 2
+ * as published by the Free Software Foundation.
+
+ * redEVENT is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with redEVENT; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+// no direct access
+defined( '_JEXEC' ) or die( 'Restricted access' );
+?>
+<div class="elcomments">
+
+<?php
+switch ($this->elsettings->commentsystem) {
+	
+	//jcomments integration
+	case 1:
+		if (file_exists(JPATH_SITE.DS.'components'.DS.'com_jcomments'.DS.'jcomments.php')) :
+			require_once(JPATH_SITE.DS.'components'.DS.'com_jcomments'.DS.'jcomments.php');
+			echo JComments::showComments($this->row->did, 'com_redevent', $this->row->title);
+		endif;
+	break;
+
+	//jomcomment integration
+	case 2:
+		if (file_exists(JPATH_SITE.DS.'plugins'.DS.'content'.DS.'jom_comment_bot.php')) :
+    		require_once(JPATH_SITE.DS.'plugins'.DS.'content'.DS.'jom_comment_bot.php');
+    		echo jomcomment($this->row->did, 'com_redevent');
+  		endif;
+	break;
+}
+?>
+</div>
