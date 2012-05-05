c01c7c8de23d02fcfc132e9316072675ec604ee9
julien <jlv@jlv-solutions.com>
2012-05-05 19:13:56 +0200

    # improve installer for updates from 2.0

--- component/install.redevent.php
+++ component/install.redevent.php
@@ -26,12 +26,60 @@ defined('_JEXEC') or die('Restricted access');
 
 class com_redeventInstallerScript
 {	
+	protected $_is15update = false;
+	
+	/**
+	 * method to run before an install/update/uninstall method
+	 *
+	 * @return void
+	 */
+	public function preflight($type, $parent)
+	{
+		// in the case of an update from 1.5 version (2.0 stable), we might have just the tables without the extension being registered
+		// in that case, we will need to run the mysql updates sql 2.5.b.2  , which was the same db as 2.0 stable		
+		$row = JTable::getInstance('extension');
+		$eid = $row->find(array('element' => 'com_redevent', 'type' => 'component'));
+		if ($eid) { // version 2.5 already installed, or migrated from jupgrade
+			$row->load($eid);
+			if ($row->manifest_cache) { // it's really an update...
+				return true;
+			}
+			// no manifest means it was migrated from 2.0 from jupgrade, be we still need to update the table from 2.0 structure
+		}
+		else {		
+			// not installed, do we have a redevent table ?
+			$tables = JFactory::getDbo()->getTableList();
+			if (!in_array(JFactory::getDbo()->getPrefix().'redevent_settings', $tables)) {				
+				// it s a clean install;
+				return true;	
+			}
+		}
+		// still here... means this is an update from 2.0
+		$this->_is15update = true;
+		return true;
+	}
+	
+	public function install()
+	{
+		if ($this->_is15update) 
+		{
+			$this->updateFrom20();
+		}
+	}
+	
+	public function update()
+	{
+		if ($this->_is15update) {
+			$this->updateFrom20();
+		}
+	}
+	
 	/**
 	* method to run after an install/update/uninstall method
 	*
 	* @return void
 	*/
-	function postflight($type, $parent)
+	public function postflight($type, $parent)
 	{
 		/* Install redform plugin */
 		jimport('joomla.filesystem.file');
@@ -54,6 +102,81 @@ class com_redeventInstallerScript
 			}
 			else {
 				echo JText::_('COM_REDEVENT_Error_publishing_redform_integration_plugin').'<br />';
+			}
+		}
+	}
+	
+	/**
+	 * method to update the database structure from a 2.0 version
+	 * 
+	 */
+	public function updateFrom20()
+	{
+		$db = &JFactory::getDbo();
+		$dbDriver = strtolower($db->name);
+		if ($dbDriver == 'mysqli')
+		{
+			$dbDriver = 'mysql';
+		}
+		elseif ($dbDriver == 'sqlsrv')
+		{
+			$dbDriver = 'sqlazure';
+		}
+		$basepath = JPATH_ADMINISTRATOR.'/components/com_redevent/sql/updates/'.$dbDriver;
+		$files = str_replace('.sql', '', JFolder::files($basepath, '\.sql$'));
+		usort($files, 'version_compare');
+		
+		if (!count($files))
+		{
+			return false;
+		}
+
+		// equivalent version
+		$version = '2.5.b.3.0';
+		if ($version)
+		{
+			// We have a version!
+			foreach ($files as $file)
+			{
+				if (version_compare($file, $version) > 0)
+				{
+					$buffer = file_get_contents($basepath . '/' . $file . '.sql');
+		
+					// Graceful exit and rollback if read not successful
+					if ($buffer === false)
+					{
+						JError::raiseWarning(1, JText::_('JLIB_INSTALLER_ERROR_SQL_READBUFFER'));
+		
+						return false;
+					}
+		
+					// Create an array of queries from the sql file
+					$queries = JInstallerHelper::splitSql($buffer);
+		
+					if (count($queries) == 0)
+					{
+						// No queries to process
+						continue;
+					}
+		
+					// Process each query in the $queries array (split out of sql file).
+					foreach ($queries as $query)
+					{
+						$query = trim($query);
+						if ($query != '' && $query{0} != '#')
+						{
+							$db->setQuery($query);
+		
+							if (!$db->query())
+							{
+								JError::raiseWarning(1, JText::sprintf('JLIB_INSTALLER_ERROR_SQL_ERROR', $db->stderr(true)));
+		
+								return false;
+							}
+						}
+					}
+					echo Jtext::sprintf('COM_REDEVENT_UPDATED_DB_TO', $file).'<br/>';
+				}
 			}
 		}
 	}
