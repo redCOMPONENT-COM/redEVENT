c15d90b2eefd07c3d07a7f5e3e9f7e9e86410ef8
julien <jlv@jlv-solutions.com>
2012-08-13 18:34:52 +0200

    + session storage for mod redeventcal toggle status

--- modules/mod_redeventcal/mod_redeventcal.js
+++ modules/mod_redeventcal/mod_redeventcal.js
@@ -2,12 +2,28 @@
 window.addEvent('domready', function(){
 	if ($$('div.redeventcal div.cal_toggle')) {
 		$$('div.redeventcal div.cal_toggle').addEvent('click', function(){
-			this.getParent('div.redeventcal').toggleClass('hide_mod');				
+			this.getParent('div.redeventcal').addClass('hide_mod');
+			if (dataStore) {
+				dataStore.setItem('hide_mod_recal', 1);
+			}
 		});
 	}
 	if ($$('div.redeventcal div.toggleoff')) {
 		$$('div.redeventcal div.toggleoff').addEvent('click', function(){
-			this.getParent('div.redeventcal').toggleClass('hide_mod');				
+			this.getParent('div.redeventcal').removeClass('hide_mod');
+			if (dataStore) {
+				dataStore.removeItem('hide_mod_recal', 1);
+			}				
 		});
+	}
+	
+	if (typeof(Storage)!=="undefined")
+	{
+		// localStorage and sessionStorage supported
+		var dataStore = window.sessionStorage;
+		
+		if (dataStore.getItem('hide_mod_recal')) {
+			$$('div.redeventcal').addClass('hide_mod');
+		}
 	}
 });
