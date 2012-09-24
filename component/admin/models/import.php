5516314a505bdb8ac91b5c2b76c06aea522be932
julien <julien@redweb.dk>
2012-09-24 17:20:46 +0200

    / added possibility to import from eventlist 1.1

--- component/admin/models/import.php
+++ component/admin/models/import.php
@@ -50,8 +50,19 @@ class RedEventModelImport extends JModel
 		parent::__construct();
 	}
   
+	/**
+	 * import categories, venues and events from eventlist
+	 */
 	public function importeventlist()
 	{
+    // find out eventlist version !
+    if (!file_exists(JPATH_ADMINISTRATOR.DS.'components'.DS.'com_eventlist'.DS.'eventlist.xml')) {
+      $this->setError(JText::_('COM_REDEVENT_EVENTLIST_NOT_FOUND'));
+      return false;    	
+    }
+		$data = JApplicationHelper::parseXMLInstallFile(JPATH_ADMINISTRATOR.DS.'components'.DS.'com_eventlist'.DS.'eventlist.xml');
+		$version = $data['version'];
+		    
 	  // make sure redevent db is empty
 	  $query = ' SELECT COUNT(*) FROM #__redevent_events ';
     $this->_db->setQuery($query);
@@ -74,7 +85,20 @@ class RedEventModelImport extends JModel
       $this->setError(JText::_('COM_REDEVENT_EVENTLIST_IMPORT_DB_NOT_EMPTY'));
       return false;
     }
-	  
+    
+    if (version_compare($version, '1.1.a') > 0) {
+    	return self::_importeventlist11();
+    }
+    else {
+    	return self::_importeventlist10();
+    }
+	}
+	
+	/**
+	 * import from eventlist 1.0 structure
+	 */
+	protected function _importeventlist10()
+	{    
 	  // import venues
 	  $query = ' INSERT IGNORE INTO #__redevent_venues (id, venue, alias, url, plz, published, state, street, city, country, locdescription, locimage, map, meta_description, meta_keywords)'
 	         . ' SELECT id, venue, alias, url, plz, published, state, street, city, country, locdescription, locimage, map, meta_description, meta_keywords FROM #__eventlist_venues '
@@ -134,7 +158,84 @@ class RedEventModelImport extends JModel
     
 	  $result = array('events' => $nb_events, 'venues' => $nb_venues, 'categories' => $nb_cats,);
 	         
-	  return $result;
+	  return $result;		
+	}
+	
+	/*
+	 * import from eventlist 1.1 db structure
+	 */
+	protected function _importeventlist11()
+	{
+	  // import venues
+	  $query = ' INSERT IGNORE INTO #__redevent_venues (
+	               id, venue, alias, url, plz, published, state, street, city, country, 
+	               latitude, longitude, ordering,
+	               locdescription, locimage, map, meta_description, meta_keywords
+	               )'
+	         . ' SELECT id, venue, alias, url, plz, published, state, street, city, country, 
+	               latitude, longitude, ordering,
+	               locdescription, locimage, map, meta_description, meta_keywords 
+	               FROM #__eventlist_venues '
+	         ;
+	  $this->_db->setQuery($query);
+	  if (!$this->_db->query()) {
+	    $this->setError(JText::_('COM_REDEVENT_EVENTLIST_ERROR_IMPORTING_VENUES'));
+	    return false;
+	  }
+	  $nb_venues = $this->_db->getAffectedRows();
+	  
+    // import categories
+    $query = ' INSERT IGNORE INTO #__redevent_categories (
+                 id, parent_id, catname, alias, published, catdescription, image, color, ordering,
+                 meta_description, meta_keywords) '
+           . ' SELECT id, parent_id, catname, alias, published, catdescription, image, color, ordering,
+                 meta_description, meta_keywords FROM #__eventlist_categories '
+           ;
+    $this->_db->setQuery($query);
+    if (!$this->_db->query()) {
+      $this->setError(JText::_('COM_REDEVENT_EVENTLIST_ERROR_IMPORTING_CATEGORIES'));
+      return false;
+    }
+    $nb_cats = $this->_db->getAffectedRows();
+    // we need to rebuild the category tree
+    $table = JTable::getInstance('RedEvent_categories', '');
+    $table->rebuildTree();
+    
+    // then import events.... We add a [eventlist_import] tag to the description, so that people don't have to manually edit each description
+    $query = ' INSERT IGNORE INTO #__redevent_events (id, title, alias, published, datdescription, summary, datimage, meta_description, meta_keywords) '
+           . ' SELECT id, title, alias, published, CONCAT("[eventlist_import]",datdescription), datdescription, datimage, meta_description, meta_keywords FROM #__eventlist_events '
+           ;
+    $this->_db->setQuery($query);
+    if (!$this->_db->query()) {
+      $this->setError(JText::_('COM_REDEVENT_EVENTLIST_ERROR_IMPORTING_EVENTS'));
+      return false;
+    }
+    $nb_events = $this->_db->getAffectedRows();
+    $this->addLibraryTag();
+        
+    // corresponding xrefs
+    $query = ' INSERT IGNORE INTO #__redevent_event_venue_xref (eventid, venueid, dates, enddates, times, endtimes, published) '
+           . ' SELECT id AS eventid, locid AS venueid, dates, enddates, times, endtimes, published FROM #__eventlist_events '
+           ;
+    $this->_db->setQuery($query);
+    if (!$this->_db->query()) {
+      $this->setError(JText::_('COM_REDEVENT_EVENTLIST_ERROR_IMPORTING_EVENTS_VENUESDATES'));
+      return false;
+    }
+    
+    // corresponding category
+    $query = ' INSERT IGNORE INTO #__redevent_event_category_xref (event_id, category_id) '
+           . ' SELECT itemid AS eventid, catid AS category_id FROM #__eventlist_cats_event_relations '
+           ;
+    $this->_db->setQuery($query);
+    if (!$this->_db->query()) {
+      $this->setError(JText::_('COM_REDEVENT_EVENTLIST_ERROR_IMPORTING_EVENTS_CATEGORIES'));
+      return false;
+    }    
+    
+	  $result = array('events' => $nb_events, 'venues' => $nb_venues, 'categories' => $nb_cats,);
+	         
+	  return $result;			
 	}
 	
 	public function addLibraryTag()
