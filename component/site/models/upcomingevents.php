8d4a95554573d457de2ca9b79f7b5b862570f93c
julien <jlv@jlv-solutions.com>
2013-03-02 12:23:31 +0100

    / frontend language filtering: upcoming events

--- component/site/models/upcomingevents.php
+++ component/site/models/upcomingevents.php
@@ -1,10 +1,9 @@
 <?php
 /**
- * @version 1.0 $Id$
- * @package Joomla
- * @subpackage redEVENT
- * @copyright redEVENT (C) 2008 redCOMPONENT.com / EventList (C) 2005 - 2008 Christoph Lukes
- * @license GNU/GPL, see LICENSE.php
+ * @package     Joomla
+ * @subpackage  redEVENT
+ * @copyright   redEVENT (C) 2008 redCOMPONENT.com / EventList (C) 2005 - 2008 Christoph Lukes
+ * @license     GNU/GPL, see LICENSE.php
  * redEVENT is based on EventList made by Christoph Lukes from schlu.net
  * redEVENT can be downloaded from www.redcomponent.com
  * redEVENT is free software; you can redistribute it and/or
@@ -21,81 +20,54 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-// no direct access
+// No direct access
 defined('_JEXEC') or die('Restricted access');
 
 jimport('joomla.application.component.model');
 
-require_once('baseeventslist.php');
+require_once 'baseeventslist.php';
+
 /**
- * EventList Component Details Model
+ * Redevent Model Upcoming events
  *
- * @package Joomla
- * @subpackage redEVENT
- * @since		0.9
+ * @package     Joomla
+ * @subpackage  redEVENT
+ * @since       0.9
  */
-class RedeventModelUpcomingevents extends RedeventModelBaseEventList {
+class RedeventModelUpcomingevents extends RedeventModelBaseEventList
+{
 	/**
-	 * Constructor
+	 * returns events
 	 *
-	 * @since 0.9
+	 * @return array
 	 */
-	function __construct() {
-		parent::__construct();
-	}
-	
-	public function getUpcomingEvents() 
+	public function getUpcomingEvents()
 	{
 		$mainframe = &JFactory::getApplication();
-		
+
 		$db = JFactory::getDBO();
 		$params = $mainframe->getParams();
-		
-		$acl = &UserAcl::getInstance();
-		$gids = $acl->getUserGroupsIds();
-		if (!is_array($gids) || !count($gids)) {
-			$gids = array(0);
+
+		$query = parent::_buildQuery();
+		$query->select('a.submission_types');
+
+		$upcoming_cond = '( ( CASE WHEN x.times THEN CONCAT(x.dates, " ", x.times) ELSE x.dates END > NOW() '
+		. ' AND x.dates < DATE_ADD(NOW(), INTERVAL ' . $params->getValue('upcoming_days_ahead', 30) . ' DAY) ) ';
+
+		if ($params->getValue('show_days_no_date', 0) == 1)
+		{
+			$upcoming_cond .= "       OR x.dates = '0000-00-00' ";
 		}
-		$gids = implode(',', $gids);
-		
-		$q = ' SELECT e.*, IF (x.course_credit = 0, "", x.course_credit) AS course_credit, x.id AS xref, '
-		   . ' x.dates, x.enddates, x.times, x.endtimes, x.icaldetails, x.icalvenue, '
-		   . ' v.venue, x.venueid, v.city AS location, v.id AS venueid,	v.country, '
-		   . ' x.title as session_title, '
-       . ' CASE WHEN CHAR_LENGTH(x.title) THEN CONCAT_WS(\' - \', e.title, x.title) ELSE e.title END as full_title, '
-       . ' CASE WHEN CHAR_LENGTH(e.alias) THEN CONCAT_WS(\':\', e.id, e.alias) ELSE e.id END as slug, '
-       . ' CASE WHEN CHAR_LENGTH(x.alias) THEN CONCAT_WS(\':\', x.id, x.alias) ELSE x.id END as xslug, '
-       . ' CASE WHEN CHAR_LENGTH(v.alias) THEN CONCAT_WS(\':\', v.id, v.alias) ELSE v.id END as venueslug '
-       
-		   . ' FROM #__redevent_venues v '
-		   . ' LEFT JOIN #__redevent_venue_category_xref AS xvcat ON v.id = xvcat.venue_id'
-		   . ' LEFT JOIN #__redevent_venues_categories AS vc ON xvcat.category_id = vc.id'
-		   . ' LEFT JOIN #__redevent_event_venue_xref x	ON x.venueid = v.id '
-		   . ' LEFT JOIN #__redevent_events e	ON x.eventid = e.id '
-		   . ' LEFT JOIN #__redevent_event_category_xref AS xcat ON xcat.event_id = e.id'
-		   . ' LEFT JOIN #__redevent_categories AS c ON c.id = xcat.category_id '
-		   
-		   . ' LEFT JOIN #__redevent_groups_venues AS gv ON gv.venue_id = v.id AND gv.group_id IN ('.$gids.')'
-		   . ' LEFT JOIN #__redevent_groups_venues_categories AS gvc ON gvc.category_id = vc.id AND gvc.group_id IN ('.$gids.')'
-		   . ' LEFT JOIN #__redevent_groups_categories AS gc ON gc.category_id = c.id AND gc.group_id IN ('.$gids.')'
-		   
-		   . ' WHERE x.published = 1 '
-		   . '   AND (v.private = 0 OR gv.id IS NOT NULL) '
-		   . '   AND (c.private = 0 OR gc.id IS NOT NULL) '
-		   . '   AND (vc.private = 0 OR vc.private IS NULL OR gvc.id IS NOT NULL) '
-		   . '   AND ( ( (CASE WHEN x.times THEN CONCAT(x.dates, " ", x.times) ELSE x.dates END) > NOW() AND x.dates < DATE_ADD(NOW(), INTERVAL '.$params->getValue('upcoming_days_ahead', 30).' DAY) ) '
-		   ;
-		if ($params->getValue('show_days_no_date', 0) == 1) {
-			$q .= "       OR x.dates = '0000-00-00' ";
-		}
-		$q .= " ) ";
-		$q .= ' GROUP BY x.id ';
-		$q .= " ORDER BY x.dates ";
-		$q .= "LIMIT ".$params->getValue('show_number_courses', 10);
-		$db->setQuery($q);
+
+		$upcoming_cond .= " ) ";
+
+		$query->where($upcoming_cond);
+
+		$db->setQuery($query, 0, $params->getValue('show_number_courses', 10));
 		$rows = $db->loadObjectList();
-		
+
 		$rows = $this->_getPrices($rows);
+
 		return $rows;
 	}
 }
--- component/site/views/upcomingevents/tmpl/default_courseinfo.php
+++ component/site/views/upcomingevents/tmpl/default_courseinfo.php
@@ -1,10 +1,9 @@
 <?php
 /**
- * @version 1.0 $Id$
- * @package Joomla
- * @subpackage redEVENT
- * @copyright redEVENT (C) 2008 redCOMPONENT.com / EventList (C) 2005 - 2008 Christoph Lukes
- * @license GNU/GPL, see LICENSE.php
+ * @package     Joomla
+ * @subpackage  redEVENT
+ * @copyright   redEVENT (C) 2008 redCOMPONENT.com / EventList (C) 2005 - 2008 Christoph Lukes
+ * @license     GNU/GPL, see LICENSE.php
  * redEVENT is based on EventList made by Christoph Lukes from schlu.net
  * redEVENT can be downloaded from www.redcomponent.com
  * redEVENT is free software; you can redistribute it and/or
@@ -21,7 +20,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-defined( '_JEXEC' ) or die( 'Restricted access' );
+defined('_JEXEC') or die('Restricted access');
 ?>
 <table>
 <thead>
@@ -31,69 +30,79 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 		<th><?php echo JText::_('COM_REDEVENT_EVENT_DATE'); ?></th>
 		<th></th>
 		<th><?php echo JText::_('COM_REDEVENT_EVENT_VENUE'); ?></th>
-		<th><?php echo JText::_('COM_REDEVENT_EVENT_PRICE'); ?></th>  
+		<th><?php echo JText::_('COM_REDEVENT_EVENT_PRICE'); ?></th>
 	</tr>
 </thead>
 <tbody>
 <?php
 $elsettings = redEVENTHelper::config();
-$imagepath = JURI::root().'administrator/components/com_redevent/assets/images/';
-foreach ($this->upcomingevents as $key => $event) {
-	$event_url = JRoute::_('index.php?option=com_redevent&view=details&xref='.$event->xref.'&id=' . $event->slug);
-	$venue_url = JRoute::_('index.php?option=com_redevent&view=venueevents&id='.$event->venueslug);
+$imagepath = JURI::root() . 'administrator/components/com_redevent/assets/images/';
+
+foreach ($this->upcomingevents as $key => $event)
+{
+	$event_url = JRoute::_('index.php?option=com_redevent&view=details&xref=' . $event->xref . '&id=' . $event->slug);
+	$venue_url = JRoute::_('index.php?option=com_redevent&view=venueevents&id=' . $event->venueslug);
 	?>
 	<tr>
 		<td><?php echo JHTML::_('link', $event_url, $event->full_title); ?></td>
-		<td><?php echo $event->location; ?></td>
-		<td><?php echo REOutput::getFlag( $event->country ); ?></td>
+		<td><?php echo $event->venue; ?></td>
+		<td><?php echo REOutput::getFlag($event->country); ?></td>
 		<td><?php echo REOutput::formatdate($event->dates, $event->times); ?></td>
 		<td><?php echo redEVENTHelper::getEventDuration($event); ?></td>
 		<td><?php echo JHTML::_('link', $venue_url, $event->venue); ?></td>
-		<td class="re-price"><?php echo REOutput::formatprices($event->prices).'/'.$event->course_credit ?></td>
+		<td class="re-price"><?php echo REOutput::formatprices($event->prices) . '/' . $event->course_credit ?></td>
 		<td>
 		<?php
 		/* Get the different submission types */
 		$submissiontypes = explode(',', $event->submission_types);
 		$venues_html = '';
-		foreach ($submissiontypes as $key => $subtype) {
-			switch ($subtype) {
+
+		foreach ($submissiontypes as $key => $subtype)
+		{
+			switch ($subtype)
+			{
 				case 'email':
-					$venues_html .= '<div class="vlink email">'.JHTML::_('link', JRoute::_('index.php?option=com_redevent&view=signup&task=signup&subtype=email&xref='.$event->xref.'&id='.$event->id), JHTML::_('image', $imagepath.$elsettings->get('signup_email_img'),  $elsettings->get('signup_email_text'), 'width="24px" height="24px"')).'</div> ';
+					$venues_html .= '<div class="vlink email">' . JHTML::_('link', JRoute::_('index.php?option=com_redevent&view=signup&task=signup&subtype=email&xref=' . $event->xref . '&id=' . $event->id), JHTML::_('image', $imagepath . $elsettings->get('signup_email_img'),  $elsettings->get('signup_email_text'), 'width="24px" height="24px"')) . '</div> ';
 					break;
+
 				case 'phone':
-					$venues_html .= '<div class="vlink phone">'.JHTML::_('link', JRoute::_('index.php?option=com_redevent&view=signup&task=signup&subtype=phone&xref='.$event->xref.'&id='.$event->id), JHTML::_('image', $imagepath.$elsettings->get('signup_phone_img'),  $elsettings->get('signup_phone_text'), 'width="24px" height="24px"')).'</div> ';
+					$venues_html .= '<div class="vlink phone">' . JHTML::_('link', JRoute::_('index.php?option=com_redevent&view=signup&task=signup&subtype=phone&xref=' . $event->xref . '&id=' . $event->id), JHTML::_('image', $imagepath . $elsettings->get('signup_phone_img'),  $elsettings->get('signup_phone_text'), 'width="24px" height="24px"')) . '</div> ';
 					break;
+
 				case 'external':
-					$venues_html .= '<div class="vlink external hasTip" title="::'.$elsettings->get('signup_external_text').'">'.JHTML::_('link', $event->submission_type_external, JHTML::_('image', $imagepath.$elsettings->get('signup_external_img'),  $elsettings->get('signup_external_text')), 'target="_blank"').'</div> ';
+					$venues_html .= '<div class="vlink external hasTip" title="::' . $elsettings->get('signup_external_text') . '">' . JHTML::_('link', $event->submission_type_external, JHTML::_('image', $imagepath . $elsettings->get('signup_external_img'),  $elsettings->get('signup_external_text')), 'target="_blank"') . '</div> ';
 					break;
+
 				case 'webform':
 					if ($event->prices && count($event->prices))
 					{
-						foreach ($event->prices as $p) 
+						foreach ($event->prices as $p)
 						{
-							$title = ' title="'.$p->name.'::'.addslashes(str_replace("\n", "<br/>", $p->tooltip)).'"';
-							$img = empty($p->image) ? JHTML::_('image', $imagepath.$elsettings->get('signup_webform_img'),  JText::_($elsettings->get('signup_webform_text')))
-							                        : JHTML::_('image', $imagepath.$p->image,  JText::_($p->name));
+							$title = ' title="' . $p->name . '::' . addslashes(str_replace("\n", "<br/>", $p->tooltip)) . '"';
+							$img = empty($p->image) ? JHTML::_('image', $imagepath . $elsettings->get('signup_webform_img'),  JText::_($elsettings->get('signup_webform_text')))
+							: JHTML::_('image', $imagepath . $p->image,  JText::_($p->name));
 							$link = JRoute::_(RedeventHelperRoute::getSignupRoute('webform', $event->slug, $event->xslug, $p->slug));
-							
-							$venues_html .= '<div class="vlink courseinfo_webform hasTip '.$p->alias.'"'.$title.'>'
-								             .JHTML::_('link', $link, $img).'</div> ';									             
+
+							$venues_html .= '<div class="vlink courseinfo_webform hasTip ' . $p->alias . '"' . $title . '>'
+							. JHTML::_('link', $link, $img) . '</div> ';
 						}
 					}
-					else {
-						$venues_html .= '<div class="vlink webform">'.JHTML::_('link', JRoute::_(RedeventHelperRoute::getSignupRoute('webform', $event->slug, $event->xslug)), JHTML::_('image', $imagepath.$elsettings->get('signup_webform_img'),  JText::_($elsettings->get('signup_webform_text')))).'</div> ';
+					else
+					{
+						$venues_html .= '<div class="vlink webform">' . JHTML::_('link', JRoute::_(RedeventHelperRoute::getSignupRoute('webform', $event->slug, $event->xslug)), JHTML::_('image', $imagepath . $elsettings->get('signup_webform_img'),  JText::_($elsettings->get('signup_webform_text')))) . '</div> ';
 					}
 					break;
+
 				case 'formaloffer':
-					$venues_html .= '<div class="vlink formaloffer">'.JHTML::_('link', JRoute::_('index.php?option=com_redevent&view=signup&subtype=formaloffer&task=signup&xref='.$event->xref.'&id='.$event->id), JHTML::_('image', $imagepath.$elsettings->get('signup_formal_offer_img'),  $elsettings->get('signup_formal_offer_text'), 'width="24px" height="24px"')).'</div> ';
+					$venues_html .= '<div class="vlink formaloffer">' . JHTML::_('link', JRoute::_('index.php?option=com_redevent&view=signup&subtype=formaloffer&task=signup&xref=' . $event->xref . '&id=' . $event->id), JHTML::_('image', $imagepath . $elsettings->get('signup_formal_offer_img'),  $elsettings->get('signup_formal_offer_text'), 'width="24px" height="24px"')) . '</div> ';
 					break;
 			}
 		}
+
 		echo $venues_html;
 		?>
 		</td>
 	</tr>
-<?php }
-?>
+<?php } ?>
 </tbody>
 </table>
