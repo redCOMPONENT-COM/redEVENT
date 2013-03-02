0114e8a890aa92709dd00ffb8f4a7810416e6aea
julien <jlv@jlv-solutions.com>
2013-03-02 11:59:17 +0100

    / frontend language filtering: venue view

--- component/site/models/venue.php
+++ component/site/models/venue.php
@@ -1,10 +1,9 @@
 <?php
 /**
- * @version 1.0 $Id: view.html.php 360 2009-06-30 07:49:16Z julien $
- * @package Joomla
- * @subpackage redEVENT
- * @copyright redEVENT (C) 2008 redCOMPONENT.com / EventList (C) 2005 - 2008 Christoph Lukes
- * @license GNU/GPL, see LICENSE.php
+ * @package     Joomla
+ * @subpackage  redEVENT
+ * @copyright   redEVENT (C) 2008 redCOMPONENT.com / EventList (C) 2005 - 2008 Christoph Lukes
+ * @license     GNU/GPL, see LICENSE.php
  * redEVENT is based on EventList made by Christoph Lukes from schlu.net
  * redEVENT can be downloaded from www.redcomponent.com
  * redEVENT is free software; you can redistribute it and/or
@@ -21,7 +20,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-// no direct access
+// No direct access
 defined('_JEXEC') or die('Restricted access');
 
 jimport('joomla.application.component.model');
@@ -29,9 +28,9 @@ jimport('joomla.application.component.model');
 /**
  * EventList Component Editvenue Model
  *
- * @package Joomla
- * @subpackage redEVENT
- * @since		0.9
+ * @package     Joomla
+ * @subpackage  redEVENT
+ * @since       0.9
  */
 class RedeventModelVenue extends JModel
 {
@@ -47,9 +46,11 @@ class RedeventModelVenue extends JModel
 	 *
 	 * @since 1.5
 	 */
-	function __construct()
+	public function __construct()
 	{
 		parent::__construct();
+
+		$this->setState('filter.language', $app->getLanguageFilter());
 
 		$id = JRequest::getInt('id');
 		$this->setId($id);
@@ -58,10 +59,11 @@ class RedeventModelVenue extends JModel
 	/**
 	 * Method to set the Venue id
 	 *
-	 * @access	public
-	 * @param	int	Venue ID number
+	 * @param   int  $id  venue id
+	 *
+	 * @return void
 	 */
-	function setId($id)
+	public function setId($id)
 	{
 		// Set new venue ID
 		$this->_id			= $id;
@@ -72,62 +74,67 @@ class RedeventModelVenue extends JModel
 	 *
 	 * @return array
 	 */
-	function &getData(  )
+	public function &getData(  )
 	{
 		$mainframe = &JFactory::getApplication();
-		
+
 		if (empty($this->_venue))
 		{
-			if ($this->_id) 
+			if ($this->_id)
 			{
 				// Load the Event data
-	      $query = ' SELECT v.id, v.venue, v.url, v.street, v.plz, v.city, v.state, v.country, v.locdescription, v.locimage, v.latitude, v.longitude, v.company, '
-				  . ' COUNT( a.id ) AS assignedevents,'
-	        . ' CASE WHEN CHAR_LENGTH(v.alias) THEN CONCAT_WS(\':\', v.id, v.alias) ELSE v.id END as slug'
-	        . ' FROM #__redevent_venues as v'
-	        . ' LEFT JOIN #__redevent_event_venue_xref AS a ON a.venueid = v.id AND a.published = 1'
-	        . ' WHERE v.id = ' . $this->_db->Quote($this->_id)
-	        . ' GROUP BY v.id '
-	        ;
+				$query = ' SELECT v.id, v.venue, v.url, v.street, v.plz, v.city, v.state, v.country, v.locdescription, v.locimage, v.latitude, v.longitude, v.company, '
+				. ' COUNT( a.id ) AS assignedevents,'
+				. ' CASE WHEN CHAR_LENGTH(v.alias) THEN CONCAT_WS(\':\', v.id, v.alias) ELSE v.id END as slug'
+				. ' FROM #__redevent_venues as v'
+				. ' LEFT JOIN #__redevent_event_venue_xref AS a ON a.venueid = v.id AND a.published = 1'
+				. ' WHERE v.id = ' . $this->_db->Quote($this->_id)
+				. ' GROUP BY v.id ';
 				$this->_db->setQuery($query);
-				
+
 				$venue = $this->_db->loadObject();
-	
-				//Create image information
+
+				// Create image information
 				$venue->limage = redEVENTImage::flyercreator($venue->locimage);
-	
-				//Generate Venuedescription
-				if (!empty ($venue->locdescription)) {
-					//execute plugins
+
+				// Generate Venuedescription
+				if (!empty ($venue->locdescription))
+				{
+					// Execute plugins
 					$venue->locdescription = JHTML::_('content.prepare', $venue->locdescription);
 				}
-	
-				//build the url
-				if(!empty($venue->url) && strtolower(substr($venue->url, 0, 7)) != "http://") {
-					$venue->url = 'http://'.$venue->url;
+
+				// Build the url
+				if (!empty($venue->url) && strtolower(substr($venue->url, 0, 7)) != "http://")
+				{
+					$venue->url = 'http://' . $venue->url;
 				}
-	
-				//prepare the url for output
-				if (strlen(htmlspecialchars($venue->url, ENT_QUOTES)) > 35) {
-					$venue->urlclean = substr( htmlspecialchars($venue->url, ENT_QUOTES), 0 , 35).'...';
-				} else {
+
+				// Prepare the url for output
+				if (strlen(htmlspecialchars($venue->url, ENT_QUOTES)) > 35)
+				{
+					$venue->urlclean = substr(htmlspecialchars($venue->url, ENT_QUOTES), 0, 35) . '...';
+				}
+				else
+				{
 					$venue->urlclean = htmlspecialchars($venue->url, ENT_QUOTES);
 				}
-	
-				//create flag
-				if ($venue->country) {
-					$venue->countryimg = REOutput::getFlag( $venue->country );
+
+				// Create flag
+				if ($venue->country)
+				{
+					$venue->countryimg = REOutput::getFlag($venue->country);
 				}
-	
-				//create target link
+
+				// Create target link
 				$venue->targetlink = JRoute::_(RedeventHelperRoute::getVenueEventsRoute($venue->slug));
-	
+
 				$venue->categories = $this->_getVenueCategories($this->_id);
-				
+
 				$this->_venue = $venue;
 			}
 		}
-		
+
 		return $this->_venue;
 	}
 
@@ -137,45 +144,53 @@ class RedeventModelVenue extends JModel
 	 * @access private
 	 * @return array
 	 */
-	function _loadVenue( )
+	protected function _loadVenue( )
 	{
-		if (empty($this->_venue)) 
+		if (empty($this->_venue))
 		{
 			$db = &JFactory::getDbo();
 			$query = $db->getQuery(true);
-			
+
 			$query->select('*');
 			$query->from('#__redevent_venues');
 			$query->where('id = ' . $this->_id);
 			$db->setQuery($query);
 			$this->_venue = $db->loadObject();
-			echo '<pre>';print_r($this->_venue); echo '</pre>';exit;
+
 			return $this->_venue;
 		}
-		return true;
+
+		return $this->_venue;
 	}
-	
 
-  /**
-   * adds categories property to event row
-   *
-   * @param object event
-   * @return object
-   */
-  function _getVenueCategories($venueid)
-  {
-    $query =  ' SELECT c.id, c.name, c.access, '
-          . ' CASE WHEN CHAR_LENGTH(c.alias) THEN CONCAT_WS(\':\', c.id, c.alias) ELSE c.id END as slug '
-          . ' FROM #__redevent_venues_categories as c '
-          . ' INNER JOIN #__redevent_venue_category_xref as x ON x.category_id = c.id '
-          . ' WHERE c.published = 1 '
-          . '   AND x.venue_id = ' . $this->_db->Quote($venueid)
-          . ' ORDER BY c.lft'
-          ;
-    $this->_db->setQuery( $query );
+	/**
+	 * adds categories property to event row
+	 *
+	 * @param   int  $venueid  venue id
+	 *
+	 * @return object
+	 */
+	protected function _getVenueCategories($venueid)
+	{
+		$db      = JFactory::getDbo();
+		$query = $db->getQuery(true);
 
-    $rows = $this->_db->loadObjectList();
+		$query->select('c.id, c.name, c.access');
+		$query->select('CASE WHEN CHAR_LENGTH(c.alias) THEN CONCAT_WS(\':\', c.id, c.alias) ELSE c.id END as slug');
+		$query->from('#__redevent_venues_categories as c');
+		$query->join('INNER', '#__redevent_venue_category_xref as x ON x.category_id = c.id');
+		$query->where('c.published = 1');
+		$query->where('x.venue_id = ' . $this->_db->Quote($venueid));
+		$query->order('c.lft');
 
-    return $rows;   
-  }
+		if ($this->getState('filter.language'))
+		{
+			$query->where('(c.language in (' . $this->_db->quote(JFactory::getLanguage()->getTag()) . ',' . $this->_db->quote('*') . ') OR c.language IS NULL)');
+		}
+
+		$db->setQuery($query);
+		$rows = $db->loadObjectList();
+
+		return $rows;
+	}
 }
