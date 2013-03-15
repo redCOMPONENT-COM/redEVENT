00d29fc223927fe0cb6856d11a8c9b5d2fec3249
julien <jlv@jlv-solutions.com>
2013-03-15 11:14:44 +0100

    / added remove obsolete file procedure in install

--- component/install.redevent.php
+++ component/install.redevent.php
@@ -25,12 +25,21 @@
 defined('_JEXEC') or die('Restricted access');
 
 class com_redeventInstallerScript
-{	
+{
     private $installed_mods             = array();
     private $installed_plugs            = array();
-    
+
 	protected $_is15update = false;
-	
+
+	/** @var array Obsolete files and folders to remove */
+	private $removeFiles = array(
+			'files'    => array(
+					'administrator/components/com_redevent/views/customfield/tmpl/form.php',
+			),
+			'folders'  => array(
+			)
+	);
+
 	/**
 	 * method to run before an install/update/uninstall method
 	 *
@@ -39,7 +48,7 @@ class com_redeventInstallerScript
 	public function preflight($type, $parent)
 	{
 		// in the case of an update from 1.5 version (2.0 stable), we might have just the tables without the extension being registered
-		// in that case, we will need to run the mysql updates sql 2.5.b.2  , which was the same db as 2.0 stable		
+		// in that case, we will need to run the mysql updates sql 2.5.b.2  , which was the same db as 2.0 stable
 		$row = JTable::getInstance('extension');
 		$eid = $row->find(array('element' => 'com_redevent', 'type' => 'component'));
 		if ($eid) { // version 2.5 already installed, or migrated from jupgrade
@@ -49,34 +58,34 @@ class com_redeventInstallerScript
 			}
 			// no manifest means it was migrated from 2.0 from jupgrade, be we still need to update the table from 2.0 structure
 		}
-		else {		
+		else {
 			// not installed, do we have a redevent table ?
 			$tables = JFactory::getDbo()->getTableList();
-			if (!in_array(JFactory::getDbo()->getPrefix().'redevent_settings', $tables)) {				
+			if (!in_array(JFactory::getDbo()->getPrefix().'redevent_settings', $tables)) {
 				// it s a clean install;
-				return true;	
+				return true;
 			}
 		}
 		// still here... means this is an update from 2.0
 		$this->_is15update = true;
 		return true;
 	}
-	
+
 	public function install()
 	{
-		if ($this->_is15update) 
+		if ($this->_is15update)
 		{
 			$this->updateFrom20();
 		}
 	}
-	
+
 	public function update()
 	{
 		if ($this->_is15update) {
 			$this->updateFrom20();
 		}
 	}
-	
+
 	/**
 	* method to run after an install/update/uninstall method
 	*
@@ -84,6 +93,9 @@ class com_redeventInstallerScript
 	*/
 	public function postflight($type, $parent)
 	{
+        // Remove obsolete files and folders
+        $this->_removeObsoleteFilesAndFolders($this->removeFiles);
+
 		$this->installModsPlugs($parent);
 		if (count($this->installed_plugs)) {
 			echo '<div>
@@ -113,7 +125,7 @@ class com_redeventInstallerScript
                          </table>
                          </div>';
 		}
-		
+
 		if (count($this->installed_mods)) {
 			echo '<div>
                           <table class="adminlist" cellspacing="1">
@@ -141,20 +153,20 @@ class com_redeventInstallerScript
 			</div>';
 		}
 	}
-	
+
 	protected function installModsPlugs($parent)
 	{
 		$manifest       = $parent->get("manifest");
 		$parent         = $parent->getParent();
 		$source         = $parent->getPath("source");
 		$db = JFactory::getDbo();
-	
+
 		//**********************************************************************
 		// DO THIS IF WE DECIDE TO AUTOINSTALL PLUGINS/MODULES
 		//**********************************************************************
 		// install plugins and modules
 		$installer = new JInstaller();
-	
+
 		// Install plugins
 		foreach($manifest->plugins->plugin as $plugin) {
 			$attributes                 = $plugin->attributes();
@@ -173,7 +185,7 @@ class com_redeventInstallerScript
 			}
 		}
 		return true;
-	
+
 		// Install modules
 		foreach($manifest->modules->module as $module) {
 			$attributes             = $module->attributes();
@@ -187,10 +199,10 @@ class com_redeventInstallerScript
 			}
 		}
 	}
-	
+
 	/**
 	 * method to update the database structure from a 2.0 version
-	 * 
+	 *
 	 */
 	public function updateFrom20()
 	{
@@ -207,7 +219,7 @@ class com_redeventInstallerScript
 		$basepath = JPATH_ADMINISTRATOR.'/components/com_redevent/sql/updates/'.$dbDriver;
 		$files = str_replace('.sql', '', JFolder::files($basepath, '\.sql$'));
 		usort($files, 'version_compare');
-		
+
 		if (!count($files))
 		{
 			return false;
@@ -215,31 +227,31 @@ class com_redeventInstallerScript
 
 		// equivalent version
 		$version = '2.5.b.3.0';
-		
+
 		// We have a version!
 		foreach ($files as $file)
 		{
 			if (version_compare($file, $version) > 0)
 			{
 				$buffer = file_get_contents($basepath . '/' . $file . '.sql');
-	
+
 				// Graceful exit and rollback if read not successful
 				if ($buffer === false)
 				{
 					JError::raiseWarning(1, JText::_('JLIB_INSTALLER_ERROR_SQL_READBUFFER'));
-	
+
 					return false;
 				}
-	
+
 				// Create an array of queries from the sql file
 				$queries = JInstallerHelper::splitSql($buffer);
-	
+
 				if (count($queries) == 0)
 				{
 					// No queries to process
 					continue;
 				}
-	
+
 				// Process each query in the $queries array (split out of sql file).
 				foreach ($queries as $query)
 				{
@@ -247,17 +259,41 @@ class com_redeventInstallerScript
 					if ($query != '' && $query{0} != '#')
 					{
 						$db->setQuery($query);
-	
+
 						if (!$db->query())
 						{
 							JError::raiseWarning(1, JText::sprintf('JLIB_INSTALLER_ERROR_SQL_ERROR', $db->stderr(true)));
-	
+
 							return false;
 						}
 					}
 				}
 				echo Jtext::sprintf('COM_REDEVENT_UPDATED_DB_TO', $file).'<br/>';
 			}
+		}
+	}
+
+	/**
+	 * Removes obsolete files and folders
+	 *
+	 * @param   array  $files  files and folders to be removed
+	 */
+	private function _removeObsoleteFilesAndFolders($files)
+	{
+		// Remove files
+		JLoader::import('joomla.filesystem.file');
+		if(!empty($files['files'])) foreach($files['files'] as $file) {
+			$f = JPATH_ROOT.'/'.$file;
+			if(!JFile::exists($f)) continue;
+			JFile::delete($f);
+		}
+
+		// Remove folders
+		JLoader::import('joomla.filesystem.file');
+		if(!empty($files['folders'])) foreach($files['folders'] as $folder) {
+			$f = JPATH_ROOT.'/'.$folder;
+			if(!JFolder::exists($f)) continue;
+			JFolder::delete($f);
 		}
 	}
 }
