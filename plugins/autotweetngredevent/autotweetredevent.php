13b3830613e6ace63a7c32628539da6fb59ef121
julien <jlv@jlv-solutions.com>
2013-04-17 15:27:41 +0200

    / fixed autotweet plugin

--- plugins/autotweetngredevent/autotweetredevent.php
+++ plugins/autotweetngredevent/autotweetredevent.php
@@ -1,7 +1,9 @@
 <?php
-/** 
- * @copyright Copyright (C) 2010 redCOMPONENT.com. All rights reserved. 
- * @license GNU/GPL, see LICENSE.php
+/**
+ * @package     RedEVENT
+ * @subpackage  RedEVENT.autotweet
+ * @copyright   Copyright (C) 2010 redCOMPONENT.com. All rights reserved.
+ * @license     GNU/GPL, see LICENSE.php
  * autotweetredevent can be downloaded from www.redcomponent.com
  * autotweetredevent is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License 2
@@ -19,134 +21,180 @@
  */
 
 // Check to ensure this file is included in Joomla!
-defined( '_JEXEC' ) or die( 'Restricted access' );
+defined('_JEXEC') or die('Restricted access');
 
 jimport('joomla.error.error');
 
 // Check for component
 if (!JComponentHelper::getComponent('com_autotweet', true)->enabled)
 {
-	JError::raiseWarning('5', 'AutoTweet NG Content-Plugin - AutoTweet NG Component is not installed or not enabled.');
-	return;
-}
-require_once JPATH_ROOT . DS . 'administrator' . DS . 'components' . DS . 'com_autotweet' . DS . 'helpers' . DS . 'autotweetbase.php';
-JTable::addIncludePath(JPATH_ROOT . DS . 'administrator' . DS . 'components' . DS . 'com_autotweet' . DS . 'tables');
+	JError::raiseWarning('5', 'AutoTweet NG Component is not installed or not enabled. - ' . __FILE__);
 
-// check for redEVENT extension
-if (!JComponentHelper::getComponent('com_redevent', true)->enabled) {
-	JError::raiseWarning('5', 'AutoTweet NG redEVENT-Plugin - redEVENT extension is not installed or not enabled.');
 	return;
 }
-// redevent
-include_once (JPATH_SITE.DS.'components'.DS.'com_redevent'.DS.'helpers'.DS.'route.php');
-require_once (JPATH_SITE.DS.'components'.DS.'com_redevent'.DS.'helpers'.DS.'log.php');
-require_once (JPATH_SITE.DS.'administrator/components/com_redevent/classes/error.class.php');
+
+include_once JPATH_ROOT . '/administrator/components/com_autotweet/helpers/autotweetbase.php';
+
+
+// Check for redEVENT extension
+if (!JComponentHelper::getComponent('com_redevent', true)->enabled)
+{
+	JError::raiseWarning('5', 'AutoTweet NG redEVENT-Plugin - redEVENT extension is not installed or not enabled.');
+
+	return;
+}
+
+// Redevent
+include_once JPATH_SITE . '/components/com_redevent/helpers/route.php';
+require_once JPATH_SITE . '/components/com_redevent/helpers/log.php';
+require_once JPATH_SITE . '/components/com_redevent/helpers/helper.php';
+require_once JPATH_SITE . '/components/com_redevent/classes/output.class.php';
+require_once JPATH_SITE . '/administrator/components/com_redevent/classes/error.class.php';
 
 /**
  * redEVENT extension plugin for AutoTweet.
  *
-  */
-class plgSystemAutotweetRedevent extends plgAutotweetBase
+ * @package     RedEVENT
+ * @subpackage  RedEVENT.autotweet
+ * @since       2.0
+ */
+class PlgSystemAutotweetRedevent extends plgAutotweetBase
 {
 	protected $text_template = '';
-	
+
 	protected $date_format = '';
-	
-	public function __construct( &$subject, $params )
+
+	/**
+	 * constructor
+	 *
+	 * @param   object  &$subject  subject
+	 * @param   array   $params    params
+	 */
+	public function __construct(&$subject, $params)
 	{
-		parent::__construct( $subject, $params );
+		parent::__construct($subject, $params);
 		$this->loadLanguage();
-		
+
 		// Get Plugin info
 		$pluginParams = $this->pluginParams;
-		
+
 		$this->text_template = $pluginParams->get('text_template', JText::_('PLG_SYSTEM_AUTOTWEET_REDEVENT_TEXT_TEMPLATE'));
 		$this->date_format   = $pluginParams->get('date_format', JText::_('PLG_SYSTEM_AUTOTWEET_REDEVENT_DATE_FORMAT'));
 	}
 
 	/**
 	 * triggered after a session gets saved
+	 *
+	 * @param   int  $xref  session id
+	 *
 	 * @return boolean
 	 */
 	public function onAfterRedeventSessionSave($xref)
 	{
-		$db = &JFactory::getDBO();
-		
-		$db = &JFactory::getDbo();
+		$db = JFactory::getDbo();
 		$query = $db->getQuery(true);
-		
-		$query->select('x.id, x.dates, x.times');
-		$query->select('e.title');
-		$query->select('v.venue');
-		$query->from('#__redevent_event_venue_xref AS x');
-		$query->join('INNER', '#__redevent_events AS e ON e.id = x.eventid');
+
+		$query->select('a.id, a.title, a.summary, a.datimage AS image');
+		$query->select('x.id as xref, x.dates, x.times, x.enddates, x.endtimes');
+		$query->select('v.venue, v.city, v.street');
+		$query->select('CASE WHEN CHAR_LENGTH(a.alias) THEN CONCAT_WS(\':\', a.id, a.alias) ELSE a.id END as slug');
+		$query->from('#__redevent_events AS a');
+		$query->join('INNER', '#__redevent_event_venue_xref AS x ON x.eventid = a.id');
 		$query->join('INNER', '#__redevent_venues AS v ON v.id = x.venueid');
-		$query->where('x.id = '.(int) $xref);
+		$query->where('x.id = ' . (int) $xref);
+
 		$db->setQuery($query);
 		$res = $db->loadObject();
-		
+
 		$date = strtotime($res->dates) ? JFactory::getDate($res->dates) : false;
-		
-		$this->postStatusMessage($res->id, JFactory::getDate()->toFormat(), $res->title.'@'.$res->venue.($date ? $date->format(' \o\n Y-m-d') : ''), 'eventsession');
-	
+
+		$this->postStatusMessage($res->id,
+			JFactory::getDate()->toFormat(),
+			$res->title . '@' . $res->venue . ($date ? $date->format(' \o\n Y-m-d') : ''),
+			'eventsession',
+			'',
+			'',
+			json_encode($res)
+		);
+
 		return true;
 	}
-	
+
 	/**
-	 * returns data to be inserted on tweeter
-	 * 
-	 * @param int $id
-	 * @param string $typeinfo
-	 * @return array title, text, hash, url
+	 * getExtendedData
+	 *
+	 * @param   string  $id              Param.
+	 * @param   string  $typeinfo        Param.
+	 * @param   string  &$native_object  Param.
+	 *
+	 * @return	array
+	 *
+	 * @since	1.5
 	 */
-	public function getData($id, $typeinfo)
-	{		
-		// $typeinfo not used
-		$db = & JFactory::getDBO();
-    $user = & JFactory::getUser();
-		
-    $query = ' SELECT a.id, a.title, a.summary, '
-           . ' x.id as xref, v.venue, x.dates, x.times, '
-           . ' CASE WHEN CHAR_LENGTH(a.alias) THEN CONCAT_WS(\':\', a.id, a.alias) ELSE a.id END as slug '
-           . ' FROM #__redevent_events AS a '
-           . ' INNER JOIN #__redevent_event_venue_xref AS x ON x.eventid = a.id '
-           . ' INNER JOIN #__redevent_venues AS v ON v.id = x.venueid '
-           . ' WHERE x.id = ' . $id;
-    $db->setQuery($query);
-    
-    if (!$event = $db->loadObject()) {
-      if ($db->getErrorNum()) {
-        RedeventError::raiseWarning('0', $db->getErrorMsg());
-      }
-    	return false;
-    }
-    
-    
-		// return values
+	public function getExtendedData($id, $typeinfo, &$native_object)
+	{
+		$event = json_decode($native_object);
+
+		// Return values
 		$data = array (
-			'title'		=> $event->title,
-			'text'		=> $this->getFulltext($event),
-			'hashtags'	=> '',
-			'url'		=> RedeventHelperRoute::getDetailsRoute($event->slug, $event->xref),
-			'fulltext' => $this->getFulltext($event),
-		);	
-		
+				'title'		=> $event->title,
+				'text'		=> $this->getFulltext($event),
+				'url'		=> JURI::root().RedeventHelperRoute::getDetailsRoute($event->slug, $event->xref),
+				'fulltext' => $this->getFulltext($event),
+				'is_valid'		=> true,
+		);
+
+		if ($event->image)
+		{
+			$data['image_url'] = JURI::root() . $event->image;
+		}
+
+		$ev = array();
+		$ev['location'] = $event->venue;
+		$ev['street'] = $event->street;
+		$ev['city'] = $event->city;
+
+		if (redEVENTHelper::isValidDate($event->dates))
+		{
+			$date = REOutput::formattime($event->dates, $event->times);
+			$ev['start_time'] = $date;
+		}
+
+		if (redEVENTHelper::isValidDate($event->enddates))
+		{
+			$date = REOutput::formattime($event->enddates, $event->endtimes);
+			$ev['end_time'] = $date;
+		}
+
+		$data['event'] = $ev;
+
 		return $data;
 	}
-	
+
+	/**
+	 * returns full text
+	 *
+	 * @param   object  $event  event data
+	 *
+	 * @return mixed
+	 */
 	protected function getFulltext($event)
 	{
 		$app = JFactory::getApplication();
 		$text = str_replace("{site_name}", $app->getCfg('sitename'), $this->text_template);
 		$text = str_replace("{title}", $event->title, $text);
 		$text = str_replace("{venue}", $event->venue, $text);
-		if (!strtotime($event->dates)) {
+
+		if (!strtotime($event->dates))
+		{
 			$text = str_replace("{date}",  '', $text);
 		}
-		else {
+		else
+		{
 			$date = JFactory::getDate($event->dates);
 			$text = str_replace("{date}",  $date->format($this->date_format), $text);
 		}
+
 		return $text;
 	}
-}	
+}
