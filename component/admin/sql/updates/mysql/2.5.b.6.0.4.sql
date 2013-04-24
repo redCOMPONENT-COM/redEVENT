a10f789edecf2b61269eaf0d2f6e35575cd35a46
julien <jlv@jlv-solutions.com>
2013-04-24 15:41:13 +0200

    init languages when updating tables

--- component/admin/sql/updates/mysql/2.5.b.6.0.4.sql
+++ component/admin/sql/updates/mysql/2.5.b.6.0.4.sql
@@ -1,1 +1,5 @@
 ALTER TABLE `#__redevent_event_venue_xref` ADD `language` char(7) NOT NULL;
+
+UPDATE `#__redevent_event_venue_xref` AS x 
+INNER JOIN `#__redevent_events` AS e ON e.id = x.eventid
+SET x.language = e.language; 
