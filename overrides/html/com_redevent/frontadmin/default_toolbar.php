215d1304aff8449a5741e8d7010a50cd9a476f2e
julien <jlv@jlv-solutions.com>
2013-10-31 11:09:53 +0100

    use https address for b2b popup

--- overrides/html/com_redevent/frontadmin/default_toolbar.php
+++ overrides/html/com_redevent/frontadmin/default_toolbar.php
@@ -23,9 +23,9 @@ defined('_JEXEC') or die('Restricted access');
 $my = JRoute::_('index.php?option=com_redmember&view=userdetail&layout=alterdetail&tmpl=component');
 // $contact = 'index.php?option=com_content&view=article&id=354&catid=73&tmpl=component';
 // $support = 'index.php?option=com_content&view=article&id=353&catid=73&tmpl=component';
-$support = 'http://www.maersktraining.com/b2b-support?tmpl=component';
-$contact = 'http://www.maersktraining.com/b2b-contact?tmpl=component';
-$filemanager =  'http://www.maersktraining.com/filemanager/file-are?tmpl=component';
+$support = 'https://www.maersktraining.com/b2b-support?tmpl=component';
+$contact = 'https://www.maersktraining.com/b2b-contact?tmpl=component';
+$filemanager =  'https://www.maersktraining.com/filemanager/file-are?tmpl=component';
 ?>
 <div id="logo" class="span4">
 	<a class="image" href="">
@@ -35,9 +35,9 @@ $filemanager =  'http://www.maersktraining.com/filemanager/file-are?tmpl=compone
 <ul class="inline">
 	<li><?php echo JHtml::link($my, JText::sprintf('COM_REDEVENT_FRONTEND_ADMIN_HELLO_USER_S', JFactory::getUser()->get('name')), array('class' => 'modal'));
 			  echo " <a>-</a> ";
-			  echo JHTML::link('index.php?option=com_users&task=user.logout', JText::_('COM_REDEVENT_FRONTEND_ADMIN_LOGOUT'));
+			  echo JHTML::link('index.php?option=com_users&task=user.logout&' . JSession::getFormToken() . '=1', JText::_('COM_REDEVENT_FRONTEND_ADMIN_LOGOUT'));
 	?></li>
-	<li><?php echo JHTML::link($my, JText::_('COM_REDEVENT_FRONTEND_ADMIN_MY_ACCOUNT'), array('class' => 'btn modal')); ?></li>
+	<li><?php echo JHTML::link($my, JText::_('COM_REDEVENT_FRONTEND_ADMIN_MY_ACCOUNT'), array('class' => 'btn myaccount', 'uid' => JFactory::getUser()->get('id'))); ?></li>
 	<li><?php echo JHTML::link($filemanager, JText::_('COM_REDEVENT_FRONTEND_ADMIN_FILE_ARCHIVE'), array('class' => 'btn modal', 'rel' => "{size: {x: 980, y:600}}")); ?></li>
 	<li><?php echo JHTML::link($support, JText::_('COM_REDEVENT_FRONTEND_ADMIN_SUPPORT'), array('class' => 'btn modal', 'rel' => "{size: {x: 780, y:600}}")); ?></li>
 	<li><?php echo JHTML::link($contact, JText::_('COM_REDEVENT_FRONTEND_ADMIN_CONTACT'), array('class' => 'btn modal', 'rel' => "{size: {x: 780, y:600}}")); ?></li>
