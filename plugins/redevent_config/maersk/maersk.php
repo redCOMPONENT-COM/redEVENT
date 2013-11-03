968f66012f62c7a5b5c06993186a35516b7c42c3
julien <jlv@jlv-solutions.com>
2013-11-03 19:58:05 +0100

    issue #MAERSK-42 added maersk redevent_config plugin for specific configuration

---
+++ plugins/redevent_config/maersk/maersk.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @package     Redevent.Plugin
+ * @subpackage  Config.Maersk
+ *
+ * @copyright   Copyright (C) 2013 redCOMPONENT.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later, see LICENSE.
+ */
+
+
+defined('JPATH_BASE') or die;
+
+// Import library dependencies
+jimport('joomla.plugin.plugin');
+
+/**
+ * Specific parameters for redEVENT.
+ *
+ * @package     Redevent.Plugin
+ * @subpackage  Config.Maersk
+ * @since       2.5
+ */
+class plgRedevent_configMaersk extends JPlugin
+{
+	/**
+	 * The plugin identifier.
+	 *
+	 * @var    string
+	 * @since  2.5
+	 */
+	protected $context = 'maersk';
+
+	/**
+	 * Alters component parameters
+	 *
+	 * @param   JRegistry  $params  parameters
+	 *
+	 * @return bool true on success
+	 */
+	public function onGetRedeventConfig(&$params)
+	{
+		$params->set('b2b_show_open', 0);
+	}
+}
