91eae541169bdc1ba00f3ca74921986c69935505
julien <jlv@jlv-solutions.com>
2014-01-30 15:24:36 +0100

    #IBCWEB-20 globase plugin

--- plugins/redevent/ibcglobase/ibcglobase.php
+++ plugins/redevent/ibcglobase/ibcglobase.php
@@ -21,6 +21,16 @@ jimport('joomla.plugin.plugin');
  */
 class plgRedeventIbcglobase extends JPlugin
 {
+	private $client;
+
+	private $ws_username;
+
+	private $ws_password;
+
+	private $listId;
+
+	private $mapping;
+
 	/**
 	 * constructor
 	 *
@@ -92,17 +102,97 @@ class plgRedeventIbcglobase extends JPlugin
 	{
 		require_once JPATH_SITE . '/components/com_redform/redform.core.php';
 
+		$this->ws_username = $this->params->get('ws_username');
+		$this->ws_password = $this->params->get('ws_password');
+		$this->listId      = (int) $this->params->get('listId');
+
 		$details = $this->getAttendeeDetails($attendeeId);
 
+		if (!$this->listId)
+		{
+			return true;
+		}
+
 		$rfcore = new RedFormCore;
-		$answers = $rfcore->getAnswers(array($details->sid));
+		$answers = $rfcore->getSidsFieldsAnswers(array($details->sid));
 
 		if ($answers)
 		{
 			$answers = reset($answers);
 		}
 
-		echo '<pre>'; echo print_r($answers, true); echo '</pre>'; exit;
+		$client = $this->getClient();
+
+		$profileFields = $client->GetListFields($this->ws_username, $this->ws_password, $this->listId);
+
+		$xmlFields = array('customfield_15' => array($details->formname));
+
+		if ($details->uddannelse)
+		{
+			$parts = explode("\n", $details->uddannelse);
+			$xmlFields['customfield_18'] = $parts;
+		}
+
+		if ($details->nyhedsbrev)
+		{
+			$parts = explode("\n", $details->nyhedsbrev);
+			$xmlFields['customfield_19'] = $parts;
+		}
+
+		foreach ($profileFields as $pf)
+		{
+			foreach ($answers as $a)
+			{
+				if ($this->getGlobaseMapping($a->id) == $pf->name)
+				{
+					if (!isset($xmlFields[$pf->name]))
+					{
+						$xmlFields[$pf->name] = array();
+					}
+
+					$xmlFields[$pf->name][] = $a->answer;
+					break;
+				}
+			}
+		}
+
+		if (!isset($xmlFields['email']) || !reset($xmlFields['email']))
+		{
+			throw new Exception('email is required');
+		}
+
+		$email = reset($xmlFields['email']);
+
+		// Is there already a profile for the email, merge special fields (Uddannelse & Nyhedsbrev)
+		if ($exists = $client->FindProfiles($this->ws_username, $this->ws_password, $this->listId, 'email', $email))
+		{
+			$previousValues = $client->GetProfileInformation($this->ws_username, $this->ws_password, $exists[0]->guid);
+
+			foreach ($previousValues->fields as $field)
+			{
+				if ($field->name == 'customfield_18' || $field->name == 'customfield_19')
+				{
+					$xmlFields[$field->name][] = $field->value;
+				}
+			}
+		}
+
+		$xml = '';
+
+		foreach ($xmlFields as $name => $values)
+		{
+			$values = array_unique($values);
+
+			foreach ($values as $v)
+			{
+				$xml .= '<' . $name  . '>' . $v . '</' . $name  . '>';
+			}
+		}
+
+		// Do the update
+		$resp = $client->SaveProfileV2(
+			$this->ws_username, $this->ws_password, $this->listId, $xml, 'email'
+		);
 	}
 
 	/**
@@ -115,15 +205,6 @@ class plgRedeventIbcglobase extends JPlugin
 	protected function getAttendeeDetails($attendeeId)
 	{
 		$app = JFactory::getApplication();
-
-		$listFieldId       = (int) $this->params->get('listFieldId');
-
-		if (!$listFieldId)
-		{
-			$app->enqueueMessage('ibcglobase plugin: missing listFieldId field id', 'error');
-
-			return false;
-		}
 
 		$uddannelseFieldId = (int) $this->params->get('uddannelseFieldId');
 
@@ -151,7 +232,10 @@ class plgRedeventIbcglobase extends JPlugin
 		$query->select('f.formname');
 
 		// Custom fields for integration
-		$query->select(array('e.custom' . $listFieldId, 'e.custom' . $uddannelseFieldId, 'e.custom' . $nyhedsbrevFieldId));
+		$query->select(array(
+			'e.custom' . $uddannelseFieldId . ' AS uddannelse',
+			'e.custom' . $nyhedsbrevFieldId . ' AS nyhedsbrev'
+		));
 
 		$query->from('#__redevent_register AS r');
 		$query->join('INNER', '#__redevent_event_venue_xref AS x ON x.id = r.xref');
@@ -164,4 +248,64 @@ class plgRedeventIbcglobase extends JPlugin
 
 		return $res;
 	}
+
+	/**
+	 * Return soap client
+	 *
+	 * @return object|SoapClient
+	 */
+	protected function getClient()
+	{
+		if (!$this->client)
+		{
+			$this->client = new SoapClient("http://ws.globase.com/v2/service.php?class=globaseSOAP&wsdl");
+		}
+
+		return $this->client;
+	}
+
+	/**
+	 * Returns the globase field name mapped to a redFORM field, if defined
+	 *
+	 * @param   int  $redformFieldId  redFORM field id
+	 *
+	 * @return bool|string the globase field name, or false if not mapped
+	 *
+	 * @throws Exception
+	 */
+	protected function getGlobaseMapping($redformFieldId)
+	{
+		if (!$this->mapping)
+		{
+			$result = array();
+			$mapping = $this->params->get('redformMapping');
+
+			if (!strstr($mapping, ';'))
+			{
+				throw new Exception('invalid mapping');
+			}
+
+			$lines = explode("\n", $mapping);
+
+			foreach ($lines as $l)
+			{
+				if ((!(strpos($l, '#') === 0)) && strstr($l, ';'))
+				{
+					list($fid, $fname) = explode(";", $l);
+					$fid = (int) $fid;
+					$fname = trim($fname);
+
+					if ($fid)
+					{
+						$result[$fid] = $fname;
+					}
+				}
+			}
+
+			$this->mapping = $result;
+		}
+
+		return isset($this->mapping[$redformFieldId]) ? $this->mapping[$redformFieldId] : false;
+	}
+
 }
