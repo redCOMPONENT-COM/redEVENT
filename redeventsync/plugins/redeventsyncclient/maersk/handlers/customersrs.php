cf379f6b2cd4ba3b0efba16411f0b8afa6e2fc5a
julien <julien@redweb.dk>
2014-07-08 10:07:57 +0200

    #MAERSK-209 fix unique username in sync

--- redeventsync/plugins/redeventsyncclient/maersk/handlers/customersrs.php
+++ redeventsync/plugins/redeventsyncclient/maersk/handlers/customersrs.php
@@ -80,9 +80,11 @@ class RedeventsyncHandlerCustomersrs extends RedeventsyncHandlerAbstractmessage
 		$data['rm_companyaddress'] = (string) $customer->CompanyAddress;
 		$data['rm_companyphone'] = (string) $customer->CompanyPhone;
 		$data['rm_companysegmentpos'] = (string) $customer->CompanySegmentPos;
-		$data['username'] = trim((string) $customer->Firstname) . trim((string) $customer->Lastname);
 		$data['name'] = trim((string) $customer->Firstname) . ' ' . trim((string) $customer->Lastname);
 		$data['email'] = (string) $customer->Emailaddress;
+
+		$data['username'] = trim((string) $customer->Firstname) . trim((string) $customer->Lastname);
+		$data['username'] = $this->getUniqueUsername($data['username']);
 
 		try
 		{
@@ -97,10 +99,32 @@ class RedeventsyncHandlerCustomersrs extends RedeventsyncHandlerAbstractmessage
 		{
 			$this->log(
 				REDEVENTSYNC_LOG_DIRECTION_INCOMING, $transaction_id,
-				$xml, 'failed', $e->getMessage());
+				$xml, 'failed', $e->getMessage()
+			);
+
 			return false;
 		}
 
 		return true;
+	}
+
+	/**
+	 * Returns unique username
+	 *
+	 * @param   string  $username  username
+	 *
+	 * @return string
+	 */
+	private function getUniqueUsername($username)
+	{
+		$res = $username;
+		$i = 1;
+
+		while (JUserHelper::getUserId($res))
+		{
+			$res = $username . '_' . ($i++);
+		}
+
+		return $res;
 	}
 }
