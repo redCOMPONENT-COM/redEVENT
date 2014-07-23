88cb56d2b46bee418aa5a3eea4221917042f67b4
julien <jlv@jlv-solutions.com>
2014-07-23 23:27:04 +0200

    #MAERSK-196 adapted org parameters

--- component/admin/sql/install.mysql.utf8.sql
+++ component/admin/sql/install.mysql.utf8.sql
@@ -359,8 +359,9 @@ CREATE TABLE IF NOT EXISTS `#__redevent_attachments` (
 CREATE TABLE IF NOT EXISTS `#__redevent_organizations` (
 `id` int(11) unsigned NOT NULL auto_increment,
 `organization_id` int(11) NOT NULL,
-`b2b_admin_notification_mailflow` tinyint(4) NOT NULL DEFAULT '0',
-`b2b_disable_all_attendee_notifications` tinyint(4) NOT NULL DEFAULT '0',
+`b2b_attendee_notification_mailflow` tinyint(4) NOT NULL DEFAULT '0',
+`b2b_attendee_notification_mailflow_orgadmin_confirmation_tag` VARCHAR(30),
+`b2b_attendee_notification_mailflow_orgadmin_cancellation_tag` VARCHAR(30),
 `b2b_cancellation_period` tinyint(4) NOT NULL DEFAULT '15',
 `checked_out` int(11) NOT NULL default '0',
 `checked_out_time` datetime NOT NULL default '0000-00-00 00:00:00',
