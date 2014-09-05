4151e5a9c46bff9ca8636e847739f4651e55ce77
julien <jlv@jlv-solutions.com>
2014-09-05 20:01:44 +0200

    MAERSK-215 added new fields in db for confirm and cancellation email subject tags

--- component/admin/sql/install.mysql.utf8.sql
+++ component/admin/sql/install.mysql.utf8.sql
@@ -361,6 +361,8 @@ CREATE TABLE IF NOT EXISTS `#__redevent_organizations` (
 `id` int(11) unsigned NOT NULL auto_increment,
 `organization_id` int(11) NOT NULL,
 `b2b_attendee_notification_mailflow` tinyint(4) NOT NULL DEFAULT '0',
+`b2b_attendee_notification_mailflow_orgadmin_confirmation_subject_tag` VARCHAR(30),
+`b2b_attendee_notification_mailflow_orgadmin_cancellation_subject_tag` VARCHAR(30),
 `b2b_attendee_notification_mailflow_orgadmin_confirmation_tag` VARCHAR(30),
 `b2b_attendee_notification_mailflow_orgadmin_cancellation_tag` VARCHAR(30),
 `b2b_cancellation_period` tinyint(4) NOT NULL DEFAULT '15',
