265a0e92540ba718b450cced3c28453d4d4e0b73
julien <jlv@jlv-solutions.com>
2014-09-22 16:24:39 +0200

    MAERSK B2B editmember form validation in override

--- overrides/html/com_redevent/frontadmin/editmember.php
+++ overrides/html/com_redevent/frontadmin/editmember.php
@@ -25,6 +25,7 @@
  */
 
 defined('_JEXEC') or die('Restricted access');
+JHtml::_('behavior.formvalidation')
 ?>
 <div class="akeeba-bootstrap">
 <?php if (!$this->modal): ?>
@@ -41,7 +42,7 @@ defined('_JEXEC') or die('Restricted access');
 		<h2><?php echo JText::_('COM_REDEVENT_FRONTEND_ADMIN_TITLE_CREATE_MEMBER'); ?></h2>
 	<?php endif; ?>
 
-	<form class="form-horizontal" id="member-update" method="post" action="index.php?option=com_redevent&controller=frontadmin&task=update_user&tmpl=component" enctype="multipart/form-data">
+	<form class="form-horizontal form-validate" id="member-update" method="post" action="index.php?option=com_redevent&controller=frontadmin&task=update_user&tmpl=component" enctype="multipart/form-data">
 
 		<div id="employee-submit">
 		<?php if (!$this->modal): ?>
