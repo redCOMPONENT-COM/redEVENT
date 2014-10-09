da7c1ff538c80e2c58696468c1837e2945c0a8e5
superfoght <bo@redweb.dk>
2014-10-09 17:45:04 +0200

    MAERSK-301 - added magnifying glass and styled input

---
+++ overrides/html/com_redevent/frontadmin/default_attendees.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * @package    Redevent
+ * @copyright  redEVENT (C) 2008 redCOMPONENT.com / EventList (C) 2005 - 2008 Christoph Lukes
+ * @license    GNU/GPL, see LICENSE.php
+ * redEVENT is based on EventList made by Christoph Lukes from schlu.net
+ * redEVENT can be downloaded from www.redcomponent.com
+ * redEVENT is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License 2
+ * as published by the Free Software Foundation.
+
+ * redEVENT is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with redEVENT; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+defined('_JEXEC') or die('Restricted access');
+?>
+
+<div id="employees-header" class="panel-heading">
+    <h2 class="panel-title">
+        <a data-toggle="collapse" data-parent="#main-results" href="#employees-result">
+            <?php echo JText::_('COM_REDEVENT_FRONTEND_ADMIN_ORG_MEMBERS_TITLE')?>
+        </a>
+    </h2>
+    <div id="search-member">
+        <input name="filter_person" id="filter_person" type="text"
+               class="input-medium form-control" placeholder="<?php echo JText::_('COM_REDEVENT_FRONTEND_ADMIN_PERSON'); ?>"
+            /><button class="btn" id="search_person" type="button"><i class="icon-search"></i></button>
+    </div>
+</div>
+
+<div id="employees-result" class="panel-collapse collapse in"></div>
+
+<script type="application/javascript">
+    <?php JHtml::script('com_redevent/autocompleter.js', false, true); ?>
+    (function(){
+        var url = '<?php echo JRoute::_('index.php?option=com_redevent&controller=frontadmin&task=personsuggestions&tmpl=component', false); ?>';
+        var completer = new Autocompleter.Request.JSON(document.id('filter_person'), url, {'postVar': 'q', 'autoSubmit': true});
+
+        completer.addEvent('onRequest', function(element, request, data){
+            data['org'] = document.id('filter_organization').get('value');
+        });
+
+        completer.addEvent('onSelection', function(element, selected){
+            document.id('filter_organization').fireEvent('change');
+        });
+    })();
+</script>
