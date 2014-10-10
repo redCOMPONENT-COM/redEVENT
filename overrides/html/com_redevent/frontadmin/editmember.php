f5278631e6158558010cb9f226221b4705e75d88
superfoght <bo@redweb.dk>
2014-10-10 15:10:01 +0200

    Fix for MAERSK-310 conflict

--- overrides/html/com_redevent/frontadmin/editmember.php
+++ overrides/html/com_redevent/frontadmin/editmember.php
@@ -27,109 +27,104 @@
 defined('_JEXEC') or die('Restricted access');
 JHtml::_('behavior.formvalidation')
 ?>
+<?php if ($this->modal): ?>
+    <script type="text/javascript">
+        window.addEvent('domready', function() {
+            document.id('cancel-employee').addEvent('click', function(){
+                window.parent.SqueezeBox.close();
+            });
+        });
+    </script>
+<?php endif; ?>
+
 <div class="akeeba-bootstrap">
 
+    <jdoc:include type="message" />
 
-<jdoc:include type="message" /> 
+    <div id="editmember-info">
 
-<div id="editmember-info">
+        <?php if ($this->uid): ?>
+            <h2><?php echo JText::_('COM_REDEVENT_FRONTEND_ADMIN_TITLE_MEMBER_INFO'); ?></h2>
+        <?php else: ?>
+            <h2><?php echo JText::_('COM_REDEVENT_FRONTEND_ADMIN_TITLE_CREATE_MEMBER'); ?></h2>
+        <?php endif; ?>
 
-	<?php if ($this->uid): ?>
-		<h2><?php echo JText::_('COM_REDEVENT_FRONTEND_ADMIN_TITLE_MEMBER_INFO'); ?></h2>
-	<?php else: ?>
-		<h2><?php echo JText::_('COM_REDEVENT_FRONTEND_ADMIN_TITLE_CREATE_MEMBER'); ?></h2>
-	<?php endif; ?>
+        <form class="form-horizontal form-validate" id="member-update" method="post" action="index.php?option=com_redevent&controller=frontadmin&task=update_user&tmpl=component" enctype="multipart/form-data">
 
-	<form class="form-horizontal form-validate" id="member-update" method="post" action="index.php?option=com_redevent&controller=frontadmin&task=update_user&tmpl=component" enctype="multipart/form-data">
+            <div id="employee-submit">
+                <?php if (!$this->modal): ?>
+                    <button type="button" class="update-employee btn"><?php echo $this->uid ? JText::_('COM_REDEVENT_UPDATE') : JText::_('COM_REDEVENT_CREATE'); ?></button>
+                    <button type="button" id="closeeditmember" class="btn"><?php echo JText::_('COM_REDEVENT_CANCEL'); ?></button>
+                <?php else: ?>
+                    <button type="submit" class="update-employee btn"><?php echo $this->uid ? JText::_('COM_REDEVENT_UPDATE') : JText::_('COM_REDEVENT_CREATE'); ?></button>
+                    <button type="button" id="cancel-employee" class="btn"><?php echo JText::_('COM_REDEVENT_CANCEL'); ?></button>
+                <?php endif; ?>
+            </div>
 
-		<div id="employee-submit">
-		<?php if (!$this->modal): ?>
-			<button type="button" class="update-employee btn"><?php echo $this->uid ? JText::_('COM_REDEVENT_UPDATE') : JText::_('COM_REDEVENT_CREATE'); ?></button>
-            <button id="closeeditmember" type="button" class="cancel-employee btn"> <?php echo JText::_('COM_REDEVENT_BACK'); ?> </button>
+            <!-- Tab panes -->
+            <div>
+                <?php foreach ($this->tabs as $t): ?>
+                    <?php foreach ($t->fields as $field): ?>
+                        <?php if ($field->id == 'name') continue; ?>
+                        <?php if ($field->id == 'password' || $field->id == 'password2') continue; ?>
+                        <?php if (!$field->hidden) : ?>
+                            <div class="control-group field-<?php echo $field->id; ?>">
+                                <?php echo $field->getLabel(array('class' => 'control-label')); ?>
+                                <div class="controls">
+                                    <?php echo $field->getInput(); ?>
+                                </div>
+                            </div>
+                        <?php else: ?>
+                            <?php echo $field->getInput(); ?>
+                        <?php endif; ?>
+                    <?php endforeach; ?>
+                <?php endforeach; ?>
+            </div>
 
-		<?php else: ?>
-			<button type="submit" class="update-employee btn"><?php echo $this->uid ? JText::_('COM_REDEVENT_UPDATE') : JText::_('COM_REDEVENT_CREATE'); ?></button>
-		<?php endif; ?>
-		</div>
+            <input type="hidden" name="modal" value="<?php echo $this->modal; ?>" />
 
-		<!-- Nav tabs -->
-		<ul class="nav nav-tabs" role="tablist">
-			<?php $i = 0; ?>
-			<?php foreach ($this->tabs as $t): ?>
-				<li class="<?php echo $i ? '' : 'active'; ?>"><a href="#tab<?php echo $i; ?>" role="tab" data-toggle="tab"><?php echo $t->tab_name; ?></a></li>
-				<?php $i++; ?>
-			<?php endforeach; ?>
-		</ul>
+        </form>
+    </div>
 
-		<!-- Tab panes -->
-		<div class="tab-content">
-			<?php $i = 0; ?>
-			<?php foreach ($this->tabs as $t): ?>
-				<div class="tab-pane<?php echo $i ? '' : ' active'; ?>" id="tab<?php echo $i; ?>">
-				<fieldset>
-					<?php foreach ($t->fields as $field): ?>
-						<?php if ($field->id == 'name') continue; ?>
-						<?php if (!$field->hidden) : ?>
-						<div class="control-group">
-							<?php echo $field->getLabel(array('class' => 'control-label')); ?>
-							<div class="controls">
-								<?php echo $field->getInput(); ?>
-							</div>
-						</div>
-						<?php else: ?>
-							<?php echo $field->getInput(); ?>
-						<?php endif; ?>
-					<?php endforeach; ?>
-				</fieldset>
-				</div>
-				<?php $i++; ?>
-			<?php endforeach; ?>
-		</div>
+    <?php if ($this->uid): ?>
+        <div id="editmember-booked">
+            <h2><?php echo JText::_('COM_REDEVENT_FRONTEND_ADMIN_MEMBER_BOOKED'); ?></h2>
+            <form class="ajaxlist">
+                <?php
+                $this->sessions = $this->booked;
+                $this->order_input = "booked_order";
+                $this->order_dir_input = "booked_order_dir";
+                $this->order = $this->booked_order;
+                $this->order_dir = $this->booked_order_dir;
+                $this->task = "getmemberbooked";
+                $this->pagination = $this->booked_pagination;
+                $this->limitstart_name = "booked_limitstart";
+                $this->limitstart = $this->booked_limitstart;
+                $this->allow_edit_sessions = true;
+                $this->show_action_column = true;
+                ?>
+                <?php echo $this->loadTemplate('sessions'); ?>
+            </form>
+        </div>
 
-		<input type="hidden" name="modal" value="<?php echo $this->modal; ?>" />
-
-    </form>
-</div>
-
-<?php if ($this->uid): ?>
-<div id="editmember-booked">
-	<h2><?php echo JText::_('COM_REDEVENT_FRONTEND_ADMIN_MEMBER_BOOKED'); ?></h2>
-	<form class="ajaxlist">
-		<?php
-			$this->sessions = $this->booked;
-			$this->order_input = "booked_order";
-			$this->order_dir_input = "booked_order_dir";
-			$this->order = $this->booked_order;
-			$this->order_dir = $this->booked_order_dir;
-			$this->task = "getmemberbooked";
-			$this->pagination = $this->booked_pagination;
-			$this->limitstart_name = "booked_limitstart";
-			$this->limitstart = $this->booked_limitstart;
-			$this->allow_edit_sessions = true;
-			$this->show_action_column = true;
-		?>
-		<?php echo $this->loadTemplate('sessions'); ?>
-	</form>
-</div>
-
-<div id="editmember-previous">
-	<h2><?php echo JText::_('COM_REDEVENT_FRONTEND_ADMIN_MEMBER_PREVIOUS'); ?></h2>
-	<form class="ajaxlist">
-		<?php
-			$this->sessions = $this->previous;
-			$this->order_input = "previous_order";
-			$this->order_dir_input = "previous_order_dir";
-			$this->order = $this->previous_order;
-			$this->order_dir = $this->previous_order_dir;
-			$this->task = "getmemberprevious";
-			$this->pagination = $this->previous_pagination;
-			$this->limitstart_name = "previous_limitstart";
-			$this->limitstart = $this->previous_limitstart;
-			$this->allow_edit_sessions = false;
-			$this->show_action_column = false;
-		?>
-		<?php echo $this->loadTemplate('sessions'); ?>
-	</form>
-</div>
-<?php endif; ?>
+        <div id="editmember-previous">
+            <h2><?php echo JText::_('COM_REDEVENT_FRONTEND_ADMIN_MEMBER_PREVIOUS'); ?></h2>
+            <form class="ajaxlist">
+                <?php
+                $this->sessions = $this->previous;
+                $this->order_input = "previous_order";
+                $this->order_dir_input = "previous_order_dir";
+                $this->order = $this->previous_order;
+                $this->order_dir = $this->previous_order_dir;
+                $this->task = "getmemberprevious";
+                $this->pagination = $this->previous_pagination;
+                $this->limitstart_name = "previous_limitstart";
+                $this->limitstart = $this->previous_limitstart;
+                $this->allow_edit_sessions = false;
+                $this->show_action_column = false;
+                ?>
+                <?php echo $this->loadTemplate('sessions'); ?>
+            </form>
+        </div>
+    <?php endif; ?>
 </div>
