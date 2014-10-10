05b8aedbe60e66c1a47ca6016e44e4f7e6d24f95
julien <jlv@jlv-solutions.com>
2014-10-10 16:21:50 +0200

    MAERSK-310 report bug cancel button move

--- component/site/views/frontadmin/tmpl/editmember.php
+++ component/site/views/frontadmin/tmpl/editmember.php
@@ -36,9 +36,6 @@ JHtml::_('behavior.formvalidation')
 <?php endif; ?>
 
 <div class="akeeba-bootstrap">
-	<?php if (!$this->modal): ?>
-		<div id="closeeditmember"><?php echo "< " . JText::_('COM_REDEVENT_BACK'); ?></div>
-	<?php endif; ?>
 
 <jdoc:include type="message" />
 
@@ -55,6 +52,7 @@ JHtml::_('behavior.formvalidation')
 		<div id="employee-submit">
 		<?php if (!$this->modal): ?>
 			<button type="button" class="update-employee btn"><?php echo $this->uid ? JText::_('COM_REDEVENT_UPDATE') : JText::_('COM_REDEVENT_CREATE'); ?></button>
+			<button type="button" id="closeeditmember" class="btn"><?php echo JText::_('COM_REDEVENT_CANCEL'); ?></button>
 		<?php else: ?>
 			<button type="submit" class="update-employee btn"><?php echo $this->uid ? JText::_('COM_REDEVENT_UPDATE') : JText::_('COM_REDEVENT_CREATE'); ?></button>
 			<button type="button" id="cancel-employee" class="btn"><?php echo JText::_('COM_REDEVENT_CANCEL'); ?></button>
