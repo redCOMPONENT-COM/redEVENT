a3f4682b42d3563d713a29d58949d1050b559baf
julien <jlv@jlv-solutions.com>
2015-01-21 09:25:20 +0100

    Remove 'all' choice in attachment access

--- component/admin/layouts/attachments/edit.php
+++ component/admin/layouts/attachments/edit.php
@@ -19,11 +19,11 @@ JHtml::_('rjquery.chosen', 'select');
 <table class="adminform" id="re-attachments">
 	<thead>
 		<tr>
-			<th style="width:25%"><?php echo JText::_('COM_REDEVENT_ATTACHMENT_FILE'); ?></th>
-			<th style="width:15%"><?php echo JText::_('COM_REDEVENT_ATTACHMENT_NAME'); ?></th>
-			<th style="width:40%"><?php echo JText::_('COM_REDEVENT_ATTACHMENT_DESCRIPTION'); ?></th>
-			<th style="width:20px"><?php echo JText::_('COM_REDEVENT_ATTACHMENT_ACCESS'); ?></th>
-			<th style="width:5px">&nbsp;</th>
+			<th><?php echo JText::_('COM_REDEVENT_ATTACHMENT_FILE'); ?></th>
+			<th><?php echo JText::_('COM_REDEVENT_ATTACHMENT_NAME'); ?></th>
+			<th><?php echo JText::_('COM_REDEVENT_ATTACHMENT_DESCRIPTION'); ?></th>
+			<th><?php echo JText::_('COM_REDEVENT_ATTACHMENT_ACCESS'); ?></th>
+			<th>&nbsp;</th>
 		</tr>
 	</thead>
 	<tbody>
@@ -32,7 +32,7 @@ JHtml::_('rjquery.chosen', 'select');
 			<td><?php echo $file->file; ?><input type="hidden" name="attached-id[]" value="<?php echo $file->id; ?>"/></td>
 			<td><input type="text" name="attached-name[]" style="width: 100%" value="<?php echo $file->name; ?>" /></td>
 			<td><input type="text" name="attached-desc[]" style="width: 100%" value="<?php echo $file->description; ?>" /></td>
-			<td><?php echo JHTML::_('access.level', 'attached-access[]', $file->access, 'class="inputbox"'); ?></td>
+			<td><?php echo JHTML::_('access.level', 'attached-access[]', $file->access, 'class="inputbox"', false); ?></td>
 			<td style="white-space: nowrap">
 				<button type="button" class="btn btn-default attach-add"><span class="icon-plus"></span></button>
 				<button type="button" class="btn btn-danger attach-remove"><span class="icon-remove"></span></button>
@@ -50,7 +50,7 @@ JHtml::_('rjquery.chosen', 'select');
 				<input type="text" name="attach-desc[]" value="" style="width: 100%" />
 			</td>
 			<td>
-				<?php echo JHTML::_('access.level', 'attach-access[]', ''); ?>
+				<?php echo JHTML::_('access.level', 'attach-access[]', '', '', false); ?>
 			</td>
 			<td style="white-space: nowrap">
 				<button type="button" class="btn btn-default attach-add"><span class="icon-plus"></span></button>
