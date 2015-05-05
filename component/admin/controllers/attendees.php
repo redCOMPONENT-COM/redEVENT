e49befcf1a4f6966a77e247582a6bd80034b9777
julien <jlv@jlv-solutions.com>
2015-05-05 19:01:10 +0200

    REDEVENT-34 missing string

--- component/admin/controllers/attendees.php
+++ component/admin/controllers/attendees.php
@@ -341,54 +341,6 @@ class RedeventControllerAttendees extends RControllerAdmin
 		$this->setRedirect($this->getRedirectToListRoute());
 	}
 
-	/* Obsolete */
-	function export()
-	{
-		$mainframe = JFactory::getApplication();
-
-		$model = $this->getModel('attendees');
-
-		$datas = $model->getData();
-
-		$doc =JFactory::getDocument();
-		$doc->setMimeEncoding('text/csv');
-		header('Expires: ' . gmdate('D, d M Y H:i:s') . ' GMT');
-		header('Content-Disposition: attachment; filename=attendees.csv');
-		header('Pragma: no-cache');
-
-		$k = 0;
-		$export = '';
-		$col = array();
-
-		for($i=0, $n=count( $datas ); $i < $n; $i++)
-		{
-			$data = &$datas[$i];
-
-    		$col[] = str_replace("\"", "\"\"", $data->name);
-    		$col[] = str_replace("\"", "\"\"", $data->username);
-    		$col[] = str_replace("\"", "\"\"", $data->email);
-    		$col[] = str_replace("\"", "\"\"", JHTML::Date( $data->uregdate, JText::_('COM_REDEVENT_JDATE_FORMAT_DATETIME' ) ));
-
-   	 		for($j = 0; $j < count($col); $j++)
-    		{
-        		$export .= "\"" . $col[$j] . "\"";
-
-        		if($j != count($col)-1)
-       	 		{
-            		$export .= ";";
-        		}
-    		}
-    		$export .= "\r\n";
-    		$col = '';
-
-			$k = 1 - $k;
-		}
-
-		echo $export;
-
-		$mainframe->close();
-	}
-
 	/**
 	 * Get the JRoute object for a redirect to list.
 	 *
