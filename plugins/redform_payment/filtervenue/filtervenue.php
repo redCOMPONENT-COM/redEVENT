e00f50d73b26f62cadd698df3afb47d7a084add0
julien <jlv@jlv-solutions.com>
2015-06-10 17:12:49 +0200

    Fixed filtervenue gateway plugin

--- plugins/redform_payment/filtervenue/filtervenue.php
+++ plugins/redform_payment/filtervenue/filtervenue.php
@@ -44,6 +44,11 @@ class plgRedform_PaymentFiltervenue extends JPlugin {
 	 */
 	public function onFilterGateways(&$gateways, $details)
 	{
+		if ($xref = JFactory::getApplication()->input->getInt('xref'))
+		{
+			return $this->filterSession($xref, $gateways);
+		}
+
 		// First check that if this is redEVENT submission
 		if (!$details->integration == 'redevent')
 		{
@@ -58,6 +63,51 @@ class plgRedform_PaymentFiltervenue extends JPlugin {
 		$query->join('INNER', '#__redevent_event_venue_xref AS x ON x.id = r.xref');
 		$query->join('INNER', '#__redevent_venues AS v ON v.id = x.venueid');
 		$query->where('r.submit_key = ' . $db->quote($details->key));
+
+		$db->setQuery($query);
+		$res = $db->loadResult();
+
+		$registry = new JRegistry;
+		$registry->loadString($res);
+
+		$allowed = $registry->get('allowed_gateways');
+
+		if (!$allowed || !count($allowed))
+		{
+			return true;
+		}
+
+		// Intersect !
+		$filtered = array();
+		foreach ($gateways as $g)
+		{
+			if (in_array($g->value, $allowed))
+			{
+				$filtered[] = $g;
+			}
+		}
+		$gateways = $filtered;
+
+		return true;
+	}
+
+	/**
+	 * Filter from session reference
+	 *
+	 * @param   int    $xref       session id
+	 * @param   array  &$gateways  gateways array
+	 *
+	 * @return bool
+	 */
+	private function filterSession($xref, &$gateways)
+	{
+		$db      = JFactory::getDbo();
+		$query = $db->getQuery(true);
+
+		$query->select('v.params');
+		$query->from('#__redevent_event_venue_xref AS x');
+		$query->join('INNER', '#__redevent_venues AS v ON v.id = x.venueid');
+		$query->where('x.id = ' . $db->quote($xref));
 
 		$db->setQuery($query);
 		$res = $db->loadResult();
