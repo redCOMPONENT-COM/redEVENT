051845d21a79b08a566471a935e123f9889e4f25
julien <jlv@jlv-solutions.com>
2015-06-18 20:55:38 +0200

    Remove call to former autocompleter script in override

--- overrides/site/html/mod_redevent_search/ajax.php
+++ overrides/site/html/mod_redevent_search/ajax.php
@@ -8,12 +8,10 @@
  * RedEvent is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License 2
  * as published by the Free Software Foundation.
-
  * RedEvent is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
-
  * You should have received a copy of the GNU General Public License
  * along with RedEvent; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
