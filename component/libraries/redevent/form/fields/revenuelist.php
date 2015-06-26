7230a5febc22c9daa209511a0ab7cfb61d58e52b
julien <jlv@jlv-solutions.com>
2015-06-26 12:43:52 +0200

    Added state filtering for venue list field

--- component/libraries/redevent/form/fields/revenuelist.php
+++ component/libraries/redevent/form/fields/revenuelist.php
@@ -23,6 +23,8 @@ class JFormFieldRevenuelist extends JFormFieldList
 	 */
 	protected $type = 'revenuelist';
 
+	protected $filter_published = '';
+
 	/**
 	 * Method to get the field options.
 	 *
@@ -32,6 +34,7 @@ class JFormFieldRevenuelist extends JFormFieldList
 	{
 		$options = array();
 		$model = RModel::getAdminInstance('Venues', array('ignore_request' => true), 'com_redevent');
+		$model->setState('filter.published', $this->filter_published);
 		$model->setState('list.ordering', 'obj.venue');
 		$model->setState('list.direction', 'asc');
 		$model->setState('list.limit', 0);
@@ -44,14 +47,42 @@ class JFormFieldRevenuelist extends JFormFieldList
 
 		$rows = $model->getItems();
 
+		$showLang = isset($this->element['show_lang']) && ($this->element['show_lang'] == 'true' || $this->element['show_lang'] == '1');
+
 		if ($rows)
 		{
 			foreach ($rows as $row)
 			{
-				$options[] = JHtml::_('select.option', $row->id, $row->venue);
+				$language = $row->language && $row->language != '*' ? $row->language : '';
+				$options[] = JHtml::_('select.option', $row->id, $showLang && $language ? $row->venue . ' (' . $language . ')' : $row->venue);
 			}
 		}
 
 		return array_merge(parent::getOptions(), $options);
+	}
+
+	/**
+	 * Method to attach a JForm object to the field.
+	 *
+	 * @param   SimpleXMLElement  $element  The SimpleXMLElement object representing the <field /> tag for the form field object.
+	 * @param   mixed             $value    The form field value to validate.
+	 * @param   string            $group    The field name group control value. This acts as as an array container for the field.
+	 *                                      For example if the field has name="foo" and the group value is set to "bar" then the
+	 *                                      full field name would end up being "bar[foo]".
+	 *
+	 * @return  boolean  True on success.
+	 */
+	public function setup(SimpleXMLElement $element, $value, $group = null)
+	{
+		if (isset($element['filter_published']))
+		{
+			$this->filter_published = (string) $element['filter_published'];
+		}
+		else
+		{
+			$this->filter_published = '';
+		}
+
+		return parent::setup($element, $value, $group);
 	}
 }
