e55ea3f0e3ea7b3745501f7953981d57659b0a65
julien <jlv@jlv-solutions.com>
2015-06-26 10:31:40 +0200

    Add Itemid to frontend edit redirect to allow for proper modules assignement

--- component/site/controllers/editevent.php
+++ component/site/controllers/editevent.php
@@ -97,4 +97,24 @@ class RedeventControllerEditevent extends RControllerForm
 
 		parent::postSaveHook($model, $validData);
 	}
+
+	/**
+	 * Gets the URL arguments to append to an item redirect.
+	 *
+	 * @param   integer  $recordId  The primary key id for the item.
+	 * @param   string   $urlVar    The name of the URL variable for the id.
+	 *
+	 * @return  string  The arguments to append to the redirect URL.
+	 */
+	protected function getRedirectToItemAppend($recordId = null, $urlVar = 'id')
+	{
+		$append = parent::getRedirectToItemAppend($recordId, $urlVar);
+
+		if ($itemId = $this->input->get('Itemid'))
+		{
+			$append .= '&Itemid=' . $itemId;
+		}
+
+		return $append;
+	}
 }
