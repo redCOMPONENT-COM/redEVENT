888c1bffd645301b49bc90e4bf95cb1af34c6f19
julien <jlv@jlv-solutions.com>
2015-10-05 10:33:34 +0200

    minor warning fix

--- component/admin/views/attendees/tmpl/default.php
+++ component/admin/views/attendees/tmpl/default.php
@@ -239,7 +239,7 @@ RHelperAsset::load('backend/attendeesmove.js');
 		<?php echo $this->pagination->getPaginationLinks(null, array('showLimitBox' => false)); ?>
 	<?php endif; ?>
 	<input type="hidden" name="task" value=""/>
-	<input type="hidden" name="xref" value="<?php echo $this->session->id; ?>"/>
+	<input type="hidden" name="xref" value="<?php echo $this->session->xref; ?>"/>
 	<input type="hidden" name="boxchecked" value="0"/>
 	<input type="hidden" name="filter_order" value="<?php echo $listOrder; ?>"/>
 	<input type="hidden" name="filter_order_Dir" value="<?php echo $listDirn; ?>"/>
