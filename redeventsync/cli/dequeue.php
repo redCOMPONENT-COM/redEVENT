fae0414d26f2bb9a300b2eadbc5d49ba19d54dc1
julien <jlv@jlv-solutions.com>
2015-11-23 09:01:30 +0100

    [fix] fixed redEVENTSYNC cli script for joomla 3

--- redeventsync/cli/dequeue.php
+++ redeventsync/cli/dequeue.php
@@ -6,40 +6,31 @@
  */
 
 // Make sure we're being called from the command line, not a web interface
-if (array_key_exists('REQUEST_METHOD', $_SERVER)) die();
+if (PHP_SAPI !== 'cli')
+{
+	die('This is a command line only application.');
+}
 
 // We are a valid entry point.
-define('_JEXEC', 1);
-define('DS', DIRECTORY_SEPARATOR);
+const _JEXEC = 1;
 
 // Load system defines
-if (file_exists(dirname(dirname(__FILE__)) . '/defines.php'))
+if (file_exists(dirname(__DIR__) . '/defines.php'))
 {
-	require_once dirname(dirname(__FILE__)) . '/defines.php';
+	require_once dirname(__DIR__) . '/defines.php';
 }
 
 if (!defined('_JDEFINES'))
 {
-	define('JPATH_BASE', dirname(dirname(__FILE__)));
+	define('JPATH_BASE', dirname(__DIR__));
 	require_once JPATH_BASE . '/includes/defines.php';
 }
 
 // Get the framework.
-require_once JPATH_LIBRARIES . '/import.php';
-require_once JPATH_LIBRARIES . '/joomla/application/component/helper.php';
+require_once JPATH_LIBRARIES . '/import.legacy.php';
 
 // Bootstrap the CMS libraries.
 require_once JPATH_LIBRARIES . '/cms.php';
-
-// Force library to be in JError legacy mode
-JError::$legacy = true;
-
-// Import necessary classes not handled by the autoloaders
-jimport('joomla.application.menu');
-jimport('joomla.environment.uri');
-jimport('joomla.event.dispatcher');
-jimport('joomla.utilities.utility');
-jimport('joomla.utilities.arrayhelper');
 
 // Import the configuration.
 require_once JPATH_CONFIGURATION . '/configuration.php';
