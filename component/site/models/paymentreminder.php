ba5a1445d8bde9a4b865ee4f07894cc78ae1c9c0
julien <jlv@jlv-solutions.com>
2015-11-25 12:50:48 +0100

    [imp] track payment reminder sent datetime per registration for #MAERSK-376

--- component/site/models/paymentreminder.php
+++ component/site/models/paymentreminder.php
@@ -16,6 +16,11 @@ defined('_JEXEC') or die('Restricted access');
 class RedeventModelPaymentreminder extends RModel
 {
 	/**
+	 * @var array
+	 */
+	private $attendees;
+
+	/**
 	 * Send reminder to attendees that didn't pay
 	 *
 	 * @param   array  $attendeeIds  attendee ids
@@ -26,12 +31,14 @@ class RedeventModelPaymentreminder extends RModel
 	 */
 	public function send($attendeeIds = null)
 	{
-		if (!$attendeeIds)
+		if ($attendeeIds)
 		{
-			$attendeeIds = $this->getAttendees();
+			$this->setAttendees($attendeeIds);
 		}
 
-		if (!count($attendeeIds))
+		$ids = array_slice($this->getAttendees(), 0, 10);
+
+		if (!count($this->getAttendees()))
 		{
 			return 0;
 		}
@@ -39,7 +46,9 @@ class RedeventModelPaymentreminder extends RModel
 		$subject = JText::_('LIB_REDEVENT_PAYMENT_REMINDER_SUBJECT');
 		$body = JText::_('LIB_REDEVENT_PAYMENT_REMINDER_BODY');
 
-		foreach ($attendeeIds as $attendeeId)
+		$sent = 0;
+
+		foreach ($ids as $attendeeId)
 		{
 			$attendee = new RedeventAttendee($attendeeId);
 
@@ -58,9 +67,35 @@ class RedeventModelPaymentreminder extends RModel
 			{
 				throw new RuntimeException('Error sending payment reminder message');
 			}
+
+			$this->updateReminderSent($attendeeId);
+			$sent++;
 		}
 
-		return count($attendeeIds);
+		return $sent;
+	}
+
+	/**
+	 * Get total of reminders to send
+	 *
+	 * @return int
+	 */
+	public function getTotal()
+	{
+		return count($this->getAttendees());
+	}
+
+	/**
+	 * set attendees
+	 *
+	 * @param   array  $ids  int array
+	 *
+	 * @return void
+	 */
+	private function setAttendees($ids)
+	{
+		JArrayHelper::toInteger($ids);
+		$this->attendees = $ids;
 	}
 
 	/**
@@ -70,18 +105,44 @@ class RedeventModelPaymentreminder extends RModel
 	 */
 	private function getAttendees()
 	{
+		$minimumDelay = (int) RedeventHelper::config()->get('payment_reminder_min_delay', 60 * 24);
+
+		if (!$this->attendees)
+		{
+			$query = $this->_db->getQuery(true)
+					->select('r.id')
+					->from('#__redevent_register AS r')
+					->join('INNER', '#__redevent_event_venue_xref AS x ON x.id = r.xref')
+					->join('INNER', '#__rwf_submitters AS s ON s.id = r.sid')
+					->join('INNER', '#__rwf_payment_request AS pr ON pr.submission_id = s.id AND paid = 0')
+					->where('(x.dates = 0 OR x.dates > NOW())')
+					->where('r.cancelled = 0')
+					->where('(r.payment_reminder_sent = 0 OR TIMESTAMPDIFF(MINUTE, NOW(), r.payment_reminder_sent) > ' . $minimumDelay . ')')
+					->where('pr.price > 0');
+
+			$this->_db->setQuery($query);
+
+			$this->attendees = $this->_db->loadColumn();
+		}
+
+		return $this->attendees;
+	}
+
+	/**
+	 * Update record
+	 *
+	 * @param   int  $attendeeId  attendee id
+	 *
+	 * @return void
+	 */
+	private function updateReminderSent($attendeeId)
+	{
 		$query = $this->_db->getQuery(true)
-				->select('r.id')
-				->from('#__redevent_register AS r')
-				->join('INNER', '#__redevent_event_venue_xref AS x ON x.id = r.xref')
-				->join('INNER', '#__rwf_submitters AS s ON s.id = r.sid')
-				->join('INNER', '#__rwf_payment_request AS pr ON pr.submission_id = s.id AND paid = 0')
-				->where('(x.dates = 0 OR x.dates > NOW())')
-				->where('r.cancelled = 0')
-				->where('pr.price > 0');
+				->update('#__redevent_register')
+				->set('payment_reminder_sent = NOW()')
+				->where('id = ' . $attendeeId);
 
 		$this->_db->setQuery($query);
-
-		return $this->_db->loadColumn();
+		$this->_db->execute();
 	}
 }
