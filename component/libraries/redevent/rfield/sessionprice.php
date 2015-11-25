41c5a6ae48908b153ab4c702d9cdf006910330ad
Mads Enemark <mads.enemark@gmail.com>
2015-11-25 16:19:03 +0100

    Fixed bug
    Fatal error: Class 'RdfLayoutHelper' not found in /home/tickcad/public_html/libraries/redevent/rfield/sessionprice.php on line 223

--- component/libraries/redevent/rfield/sessionprice.php
+++ component/libraries/redevent/rfield/sessionprice.php
@@ -220,7 +220,7 @@ class RedeventRfieldSessionprice extends RdfRfieldRadio
 	{
 		$data = $this->load();
 
-		$element = RdfLayoutHelper::render(
+		$element = RedeventLayoutHelper::render(
 			'rform.rfield.' . $this->type,
 			$this,
 			'',
