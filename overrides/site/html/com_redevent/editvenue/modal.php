d5dc0f550d31894c8b6aa4ffe408f891b5974b4a
trangredweb <trang@redweb.dk>
2016-02-16 17:22:02 +0700

    Trang#override_edit_venue_ITFF

--- overrides/site/html/com_redevent/editvenue/modal.php
+++ overrides/site/html/com_redevent/editvenue/modal.php
@@ -10,17 +10,13 @@ JHTML::_('behavior.formvalidation');
 JHtml::_('rjquery.chosen', 'select');
 JHtml::_('formbehavior.chosen', 'select');
 
-JFactory::getDocument()->addScriptDeclaration("
-	Joomla.submitbutton = function(task)
-	{
-		if (task == 'editvenue.cancel' || document.formvalidator.isValid(document.getElementById('adminForm')))
-		{
-			" . $this->form->getField('locdescription')->save() . "
-			Joomla.submitform(task);
-		}
-	};
-");
 $fieldId = JFactory::getApplication()->input->get('fieldId');
+
+if (!$this->form->getValue('country'))
+{
+	$this->form->setValue('country', null, 'DK');
+}
+$this->form->setValue('categories', null, '1');
 ?>
 
 <script type="text/javascript">
@@ -33,34 +29,103 @@ $fieldId = JFactory::getApplication()->input->get('fieldId');
 				jQuery(this).find("label").off('click');
 			}
 		});
-		//jQuery("select").select2();
+		
+		var maxlengths;
+		var controls;
+		var findelement;
+		var text_placeholder;
+		function set_max_limit_for_element(inputs,maxlengths,controls,findelement)
+		{
+			jQuery(inputs).attr('maxlength',maxlengths );
+			/*Set limit text when keyup*/
+			jQuery(inputs).keyup(function () {
+			var left = maxlengths - jQuery(this).val().length;
+			if (left < 0) {
+				left = 0;
+			}
+			jQuery(controls).find('.charleft').text( left);
+			});
+
+			if(jQuery(controls).find('input').val()=='')
+			{
+				jQuery(controls).find('.charleft').text( maxlengths);
+			}
+			else
+			{
+				var length_title_input=jQuery(controls).find(findelement).val().length;
+				var length_title_left=maxlengths - length_title_input;
+				jQuery(controls).find('.charleft').text( length_title_left);
+			}
+
+		}
+		function set_placeholder(input,text_placeholder)
+		{
+			jQuery(input).attr('placeholder',text_placeholder) ;
+		}
+		// Disable click function on btn-group
+		jQuery(".btn-group").each(function(index){
+			if (jQuery(this).hasClass('disabled'))
+			{
+				jQuery(this).find("label").off('click');
+			}
+		});
+		//jQuery('select').select2();
+		set_max_limit_for_element('.edit-venue .jform_venue input',50,'.edit-venue .jform_venue .controls ','input');
+		set_max_limit_for_element('.edit-venue .jform_locdescription textarea',150,'.edit-venue .jform_locdescription .controls ','textarea');
+		set_placeholder('.edit-venue .jform_venue .controls input','Mødested');
+		set_placeholder('.edit-venue .jform_street input','Gade');
+		set_placeholder('.edit-venue .jform_plz input','Postnummer');
+		set_placeholder('.edit-venue .jform_city input','By');
+		set_placeholder('.edit-venue .jform_url input','Webadressen skal være i dette format -http://www.google.dk');
+		set_placeholder('.edit-venue .jform_company .controls input','Virksomhed knyttet til dette mødested');
+		jQuery('.edit-venue .jform_locdescription textarea').attr('maxlength', '150');
 	});
+
+	function venueSubmit(task)
+	{
+		if (task == 'editvenue.cancel' || document.formvalidator.isValid(document.getElementById('adminForm')))
+		{
+			Joomla.submitform(task);
+		}
+	}
 </script>
 
-
-<form enctype="multipart/form-data" class="edit-venue"
+<form enctype="multipart/form-data" class="edit-venue form-validate"
       action="<?php echo JRoute::_('index.php?option=com_redevent&tmpl=component&modal=1&view=editvenue&id=' . $this->item->id); ?>"
-      method="post" name="adminForm" class="form-validate"
+      method="post" name="adminForm" class=""
       id="adminForm">
 
-	
+
 	
 	<fieldset class="form-horizontal">
-		<?php foreach ($this->form->getFieldset('venue') as $field) : 
-			if($field->name!='jform[alias]' && $field->name!='jform[venue_code]' && $field->name!='jform[published]'
-				&& $field->name!='jform[language]' && $field->name!='jform[access]' 
-				):
-			?>
-			
-			<div class="control-group  field">
+		<?php foreach ($this->form->getFieldset('venue') as $field) :
+		//print_r($field);die();
+			if ($field->name == 'jform[locdescription]'): ?>
+			<div class="control-group  field <?php echo $field->id ?>">
 				<div class="control-label">
 					<?php echo $field->label; ?>
-					
+				</div>
+				<div class="controls">
+					<textarea name="jform[locdescription]"
+					          class="locdescription" cols="50" rows="2"><?php echo $this->item->locdescription; ?></textarea>
+					<div style="clear:both;" class="charleft badge shl-char-counter-title-joomla-be badge-success" title="Show recommended character count: stay green!"></div>
+
+				</div>
+			</div>
+			<?php elseif($field->name!='jform[alias]' && $field->name!='jform[venue_code]' && $field->name!='jform[published]'
+				&& $field->name!='jform[language]' && $field->name!='jform[access]' && $field->name!='jform[status]' && $field->name!='jform[locimage]'
+				):
+			?>
+			<div class="control-group  field <?php echo $field->id ?>">
+				<div class="control-label">
+					<?php echo $field->label; ?>
 				</div>
 				<div class="controls">
 				<?php echo $field->input; ?>
 				<?php if($field->required=='true'):?>
 					<span class="required">*</span>
+					<div style="clear:both;" class="charleft badge shl-char-counter-title-joomla-be badge-success" title="Show recommended character count: stay green!"></div>
+
 				<?php endif;?>
 				</div>
 			</div>
@@ -68,8 +133,13 @@ $fieldId = JFactory::getApplication()->input->get('fieldId');
 	</fieldset>
 
 	<fieldset class="form-horizontal">
-		<?php foreach ($this->form->getFieldset('address') as $field) : ?>
-			<div class="control-group">
+		<?php foreach ($this->form->getFieldset('address') as $field) :
+			if ($field->name == 'jform[locdescription]'): ?>
+				<textarea name="jform[locdescription]" class="locdescription">
+					<?php echo $this->item->locdescription; ?>
+				</textarea>
+			<?php elseif ($field->name != 'jform[state]' && $field->name!='jform[latitude]' && $field->name!='jform[longitude]' && $field->name!='jform[map]'): ?>
+			<div class="control-group <?php echo $field->id ?>">
 				<div class="control-label">
 					<?php echo $field->label; ?>
 				</div>
@@ -77,36 +147,32 @@ $fieldId = JFactory::getApplication()->input->get('fieldId');
 					<?php echo $field->input; ?>
 				</div>
 			</div>
-		<?php endforeach; ?>
-		<div class="control-group">
+		<?php endif; endforeach; ?>
+		<div class="control-group hidden">
 			<div class="control-label"></div>
 			<div class="controls">
 				<?php echo RedeventHelperOutput::pinpointicon($this->item); ?>
 			</div>
 		</div>
 	</fieldset>
-
-	<!-- <button type="button" class="btn btn-primary" onclick="Joomla.submitbutton('editvenue.save')">
-		<span class="icon-ok"></span>&#160;<?php echo JText::_('JSAVE') ?>
-	</button>
-	<button type="button" class="btn" onclick="Joomla.submitbutton('editvenue.cancel')">
-		<span class="icon-cancel"></span>&#160;<?php echo JText::_('JCANCEL') ?>
-	</button> -->
+	<div class="row">
 	<div class="btn-toolbar">
 		<div class="btn-group">
-			<button type="button" class="btn btn-primary btn-save-event" onclick="Joomla.submitbutton('editvenue.save')">
+			<button type="submit" class="btn btn-primary btn-save-event" onclick="venueSubmit('editvenue.save')">
 				<span class="icon-ok"></span>&#160;<?php echo JText::_('JSAVE') ?>
 			</button>
 		</div>
 		<div class="btn-group">
-			<button type="button" class="btn btn-cancel-event" onclick="Joomla.submitbutton('editvenue.cancel')">
+			<button type="cancel" class="btn btn-cancel-event" onclick="venueSubmit('editvenue.cancel')">
 				<span class="icon-cancel"></span>&#160;<?php echo JText::_('JCANCEL') ?>
 			</button>
 		</div>
+	</div>
 	</div>
 	<?php echo $this->form->getInput('id'); ?>
 	<input type="hidden" name="option" value="com_redevent" />
 	<input type="hidden" name="task" value="" />
 	<input type="hidden" name="fieldId" value="<?php echo $fieldId; ?>" />
+	<input type="hidden" name="modal" value="1" />
 	<?php echo JHtml::_('form.token'); ?>
 </form>
