8fe9423ac945b393f88fa6026957417383adcef2
julien <jlv@jlv-solutions.com>
2016-05-30 09:58:30 +0200

    [fix] added rss feedcal and ical on day view, closes #REDEVENT-18

---
+++ component/site/views/day/view.raw.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * @package    Redevent.Site
+ *
+ * @copyright  Copyright (C) 2008 - 2015 redCOMPONENT.com. All rights reserved.
+ * @license    GNU General Public License version 2 or later, see LICENSE.
+ */
+
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * ICS View class
+ *
+ * @package  Redevent.Site
+ * @since    3.0
+ */
+class RedeventViewDay extends RViewSite
+{
+	/**
+	 * Execute and display a template script.
+	 *
+	 * @param   string  $tpl  The name of the template file to parse; automatically searches through the template paths.
+	 *
+	 * @return  mixed  A string if successful, otherwise a Error object.
+	 */
+	public function display($tpl = null)
+	{
+		$mainframe = JFactory::getApplication();
+
+		$settings = RedeventHelper::config();
+
+		// Get data from the model
+		$model = $this->getModel();
+		$model->setLimit($settings->get('ical_max_items', 100));
+		$model->setLimitstart(0);
+		$rows = $model->getData();
+
+		// Initiate new CALENDAR
+		$vcal = RedeventHelper::getCalendarTool();
+		$vcal->setProperty('unique_id', 'allevents@' . $mainframe->getCfg('sitename'));
+		$vcal->setConfig("filename", "events.ics");
+
+		foreach ($rows as $row)
+		{
+			RedeventHelper::icalAddEvent($vcal, $row);
+		}
+
+		$vcal->returnCalendar();
+
+		$mainframe->close();
+	}
+}
