f356ba9c7beb7183aa50dec2e9156e9b5b64364b
julienV <jlv@jlv-solutions.com>
2016-12-22 19:21:28 +0100

    [imp] added ->categories to venue twig entity, fixes #MT16-274

---
+++ component/libraries/redevent/entity/venuescategory.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package     Redevent.Library
+ * @subpackage  Entity
+ *
+ * @copyright   Copyright (C) 2008 - 2015 redCOMPONENT.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+defined('_JEXEC') or die;
+
+/**
+ * Venue Category entity.
+ *
+ * @since  3.2.2
+ */
+class RedeventEntityVenuescategory extends RedeventEntityBase
+{
+}
