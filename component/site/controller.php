26df922148dc897779d239355d1ce649b9b2d142
julien <julien.vonthron@gmail.com>
2017-01-31 14:28:43 +0100

    [fix] fixed attachements download

--- component/site/controller.php
+++ component/site/controller.php
@@ -210,50 +210,6 @@ class RedeventController extends RedeventControllerFront
 	}
 
 	/**
-	 * for attachement downloads
-	 *
-	 * @return void
-	 */
-	public function getfile()
-	{
-		$app = JFactory::getApplication();
-		$id = $app->input->getInt('file', 0);
-		$user = JFactory::getUser();
-		$helper = new RedeventHelperAttachment;
-		$path = $helper->getAttachmentPath($id, max($user->getAuthorisedViewLevels()));
-
-		// The header is fine tuned to work with grump ie8... if you modify a property, make sure it's still ok !
-		header('Content-Description: File Transfer');
-
-		// Mime
-		$mime = RedeventHelper::getMime($path);
-		$doc = JFactory::getDocument();
-		$doc->setMimeEncoding($mime);
-
-		header('Content-Disposition: attachment; filename="' . basename($path) . '"');
-		header('Content-Transfer-Encoding: binary');
-		header('Expires: 0');
-		header('Cache-Control: no-store, no-cache');
-		header('Pragma: no-cache');
-
-		if ($fd = fopen($path, "r"))
-		{
-			$fsize = filesize($path);
-			header("Content-length: $fsize");
-
-			while (!feof($fd))
-			{
-				$buffer = fread($fd, 2048);
-				echo $buffer;
-			}
-		}
-
-		fclose($fd);
-
-		return;
-	}
-
-	/**
 	 * Task handler
 	 *
 	 * @return void
