a3ff97b1d2bf8231539ff63d4b5525d687618d3e
julien <julien.vonthron@gmail.com>
2017-02-20 13:45:43 +0100

    [fix] fixed saving session in frontend

--- component/site/views/editsession/tmpl/edit_prices.php
+++ component/site/views/editsession/tmpl/edit_prices.php
@@ -7,35 +7,4 @@
 
 defined('_JEXEC') or die('Restricted access');
 
-RHelperAsset::load('xref_prices.js');
-?>
-<table class="adminform" id="re-prices">
-	<?php if ($this->prices): ?>
-	<?php foreach ((array) $this->prices as $k => $r): ?>
-		<tr>
-			<td>
-				<?php echo JHTML::_('select.genericlist', $this->pricegroupsoptions, 'jform[pricegroup][]', '', 'value', 'text', $r->pricegroup_id); ?>
-			</td>
-			<td>
-				<input type="text" name="jform[price][]" class="price-val" value="<?php echo $r->price; ?>"/>
-				<?php echo JHTML::_('select.genericlist', $this->currencyoptions, 'jform[currency][]', '', 'value', 'text', $r->currency); ?>
-			</td>
-			<td>
-				<button type="button" class="btn price-button remove-price"><?php echo Jtext::_('COM_REDEVENT_REMOVE'); ?></button>
-			</td>
-		</tr>
-	<?php endforeach; ?>
-	<?php endif; ?>
-	<tr id="trnewprice">
-		<td>
-			<?php echo JHTML::_('select.genericlist', $this->pricegroupsoptions, 'jform[pricegroup][]', array('id' => 'newprice', 'class' => 'price-group'), 'value', 'text'); ?>
-		</td>
-		<td>
-			<input type="text" name="jform[price][]" class="price-val" value="0.00" size="10" />
-			<?php echo JHTML::_('select.genericlist', $this->currencyoptions, 'jform[currency][]', array('class' => 'price-currency'), 'value', 'text'); ?>
-		</td>
-		<td>
-			<button type="button" class="btn price-button" id="add-price"><?php echo JText::_('COM_REDEVENT_add'); ?></button>
-		</td>
-	</tr>
-</table>
+echo RedeventLayoutHelper::render('redevent.prices.edit', $this, null, array('component' => 'com_redevent'));
