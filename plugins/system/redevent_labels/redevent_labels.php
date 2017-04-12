09a5d9bd74a83f43b0fce3837da95ffa892a8430
julien <julien.vonthron@gmail.com>
2017-04-12 13:54:24 +0200

    [imp] working labels

--- plugins/system/redevent_labels/redevent_labels.php
+++ plugins/system/redevent_labels/redevent_labels.php
@@ -11,6 +11,9 @@ defined('JPATH_BASE') or die;
 // Import library dependencies
 jimport('joomla.plugin.plugin');
 
+require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/labels.php';
+
 /**
  * redEVENT labels plugin
  *
@@ -43,8 +46,10 @@ class PlgSystemRedevent_Labels extends JPlugin
 		switch ($task)
 		{
 			case 'attendees.labels':
-				exit('TBD');
+				return $this->getPdf();
 		}
+
+		return;
 	}
 
 	/**
@@ -71,4 +76,129 @@ class PlgSystemRedevent_Labels extends JPlugin
 			$toolbar->addGroup($group);
 		}
 	}
+
+	/**
+	 * Return associated attendees
+	 *
+	 * @return RedeventEntityAttendee[]
+	 */
+	private function getAttendees()
+	{
+		$session = $this->getSession();
+
+		if (!$attendees = $session->getAttendees())
+		{
+			return false;
+		}
+
+		return array_filter(
+			$attendees,
+			function ($attendee)
+			{
+				return 0 == $attendee->waiting_list && 0 == $attendee->cancelled && 1 == $attendee->confirmed;
+			}
+		);
+	}
+
+	/**
+	 * Get session
+	 *
+	 * @return RedeventEntitySession
+	 *
+	 * @since __deploy_version__
+	 */
+	private function getSession()
+	{
+		$app = JFactory::getApplication();
+		$input = $app->input;
+
+		$xref = $input->getInt('xref');
+
+		return RedeventEntitySession::load($xref);
+	}
+
+	/**
+	 * Get the pdf
+	 *
+	 * @return void
+	 *
+	 * @since __deploy_version__
+	 */
+	private function getPdf()
+	{
+		$session = $this->getSession();
+		$attendees = $this->getAttendees();
+
+		$rawText = $this->getTextFromTextlibrary($this->params->get('label_content'));
+
+		$format = $this->getFormat();
+
+		$pdf = new RedeventLabelLabels($format);
+		$pdf->SetTitle($session->getFullTitle());
+
+		$pdf->AddPage();
+
+		foreach ($attendees as $attendee)
+		{
+			$text = $attendee->replaceTags($rawText);
+			$pdf->Add_Label($text);
+		}
+
+		$pdf->Output(JFile::makeSafe($session->getFullTitle() . '.pdf'), 'I');
+		JFactory::getApplication()->close();
+	}
+
+	/**
+	 * Get base text from text library
+	 *
+	 * @param   string  $tagName  tag name
+	 *
+	 * @return object
+	 *
+	 * @since __deploy_version__
+	 */
+	private function getTextFromTextlibrary($tagName)
+	{
+		$db    = JFactory::getDbo();
+		$query = $db->getQuery(true)
+			->select('text_field')
+			->from('#__redevent_textlibrary')
+			->where('text_name = ' . $db->q($tagName));
+
+		$db->setQuery($query);
+
+		 return $db->loadResult();
+	}
+
+	/**
+	 * Get format
+	 *
+	 * @return array|mixed
+	 *
+	 * @since __deploy_version__
+	 */
+	private function getFormat()
+	{
+		$format = $this->params->get('print_layout');
+
+		if (!'custom' == $format)
+		{
+			return $format;
+		}
+
+		return array(
+			'paper-size' => $this->params->get('custom_paper_size'),
+			'metric' => 'mm',
+			'marginLeft' => $this->params->get('custom_marginLeft'),
+			'marginTop' => $this->params->get('custom_marginTop'),
+			'NX' => $this->params->get('custom_nx'),
+			'NY' => $this->params->get('custom_ny'),
+			'SpaceX' => $this->params->get('custom_SpaceX'),
+			'SpaceY' => $this->params->get('custom_SpaceY'),
+			'width' => $this->params->get('custom_width'),
+			'height' => $this->params->get('custom_height'),
+			'font-size' => $this->params->get('custom_font_size'),
+		);
+	}
+
 }
