fac173f4d43530e4ee3c72f114b44e22bb77f4f0
julienV <jlv@jlv-solutions.com>
2017-09-27 10:58:45 +0200

    test html

--- tests/acceptance/install/01-InstallJoomlaCest.php
+++ tests/acceptance/install/01-InstallJoomlaCest.php
@@ -11,7 +11,7 @@ class InstallJoomlaCest
 	public function installJoomla(\AcceptanceTester $I)
 	{
 		$I->wantToTest('Joomla 3 Installation');
-		$I->amOnUrl('http://localhost/tests/index.php');
+		$I->amOnUrl('http://localhost/tests/index.html');
 		$I->seeElement(['id' => 'dummy']);
 		$I->installJoomlaRemovingInstallationFolder();
 		$I->doAdministratorLogin();
