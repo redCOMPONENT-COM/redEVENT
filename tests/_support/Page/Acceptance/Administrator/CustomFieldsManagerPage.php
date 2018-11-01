7570c8c964bc37b0b272203e5311658b6ecc53fb
unknown <trang.nguyen@redweb.dk>
2018-11-01 14:56:19 +0700

    edit namespace in file CustomFieldsManagerPage.php

--- tests/_support/Page/Acceptance/Administrator/CustomFieldsManagerPage.php
+++ tests/_support/Page/Acceptance/Administrator/CustomFieldsManagerPage.php
@@ -6,7 +6,7 @@
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-namespace Page\acceptance\administrator;
+namespace Page\Acceptance\Administrator;
 
 class CustomFieldsManagerPage extends AbstractPage
 {
