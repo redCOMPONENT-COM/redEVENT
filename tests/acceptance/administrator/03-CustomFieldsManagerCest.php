feef730fbfced3363eec50e1a4ac0258e609313a
unknown <trang.nguyen@redweb.dk>
2018-11-01 14:11:12 +0700

    refactor class CustomField

---
+++ tests/acceptance/administrator/03-CustomFieldsManagerCest.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ * @package     Redevent
+ * @subpackage  Tests
+ * @copyright   Copyright (C) 2008 - 2018 redCOMPONENT.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+use Faker\Generator;
+use Faker\Factory;
+use Page\Acceptance\Administrator\AbstractPage;
+use Page\Acceptance\Administrator\CustomFieldsManagerPage;
+use Step\Acceptance\Administrator\CustomFieldsManagerSteps;
+
+class CreateCustomFieldsCest
+{
+	/**
+	 * @var   Generator
+	 * @since 1.0.0
+	 */
+	protected $faker;
+
+	/**
+	 * @var   string
+	 * @since 1.0.0
+	 */
+	protected $CustomFieldName1;
+
+	/**
+	 * @var   string
+	 * @since 1.0.0
+	 */
+	protected $CustomFieldName2;
+
+	/**
+	 * CategoryManagerCest constructor.
+	 *
+	 * @since 1.0.0
+	 */
+	public function __construct()
+	{
+		$this->faker       = Factory::create();
+		$this->CustomFieldName1 = $this->faker->bothify("some text ##??");
+
+		$this->CustomFieldName2 = $this->faker->bothify("some text for session ##??");
+	}
+
+	public function _before(\AcceptanceTester $i)
+	{
+		$i->doAdministratorLogin();
+	}
+
+	/**
+	 * @param CustomFieldsManagerSteps $I
+	 * @throws Exception
+	 */
+	public function addEventTextField(CustomFieldsManagerSteps $I)
+	{
+		$I->wantToTest('Add an event custom text field in redEVENT');
+		$I->createCustomFieldNew(
+			array(
+				'name' => $this->CustomFieldName1,
+				'object' => 'event',
+				'type' => 'Text'
+			)
+		);
+		$I->waitForText(AbstractPage::$messageSaveSuccess, 30, AbstractPage::$message);
+		$I->wantToTest('Search Custom Fields in redEVENT');
+		$I->SearchCustomField($this->CustomFieldName1);
+		$I->see("Event", CustomFieldsManagerPage::$objectResult);
+		$I->see("text", CustomFieldsManagerPage::$typeResult);
+	}
+
+	/**
+	 * @param CustomFieldsManagerSteps $I
+	 * @throws Exception
+	 */
+	public function addSessionTextField(CustomFieldsManagerSteps $I)
+	{
+		$I->wantToTest('Add an session custom text field in redEVENT');
+		$I->createCustomField(
+			array(
+				'name' => $this->CustomFieldName2,
+				'object' => 'Session',
+				'type' => 'Text'
+			)
+		);
+		$I->waitForText(AbstractPage::$messageSaveSuccess, 30, AbstractPage::$message);
+		$I->wantToTest('Search Custom Fields in redEVENT');
+		$I->SearchCustomField($this->CustomFieldName2);
+		$I->see("Session",CustomFieldsManagerPage::$objectResult);
+		$I->see("text", CustomFieldsManagerPage::$typeResult);
+	}
+	/**
+	 * @param CustomFieldsManagerSteps $I
+	 * @throws Exception
+	 */
+	public function AllCaseCategory(CustomFieldsManagerSteps $I)
+	{
+		$I->SearchCustomField($this->CustomFieldName1);
+		$I->dontSee($this->CustomFieldName2);
+		$I->buttonClear($this->CustomFieldName1,$this->CustomFieldName2);
+		$I->wantToTest('delete a Custom Field 1 in redEVENT');
+		$I->deleteCustomFields($this->CustomFieldName1);
+		$I->wantToTest('delete a Custom Field 2 in redEVENT');
+		$I->deleteCustomFields($this->CustomFieldName2);
+
+	}
+}
