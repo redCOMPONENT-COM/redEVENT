70f17df7b8a9c9b16c65f36cb4adec834e37306a
unknown <trang.nguyen@redweb.dk>
2018-11-02 11:31:58 +0700

    refactor for class /Bundle

---
+++ tests/_support/Step/Acceptance/Administrator/BundleManagerSteps.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * @package     Redevent
+ * @subpackage  Tests
+ * @copyright   Copyright (C) 2008 - 2018 redCOMPONENT.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+namespace Step\Acceptance\Administrator;
+use \Page\Acceptance\Administrator\BundleManagerPage;
+use Step\Acceptance\AdminRedevent;
+
+class BundleManagerSteps extends AdminRedevent
+{
+	/**
+	 * @param $bundleName
+	 * @throws \Exception
+	 */
+	public function createBundleNew($bundleName)
+	{
+		$I = $this;
+		$I->createItemNew(BundleManagerPage::$URL,BundleManagerPage::$bundleTitle,BundleManagerPage::$bundleTitleNew,$bundleName);
+	}
+
+	/**
+	 * @param $bundleName
+	 * @throws \Exception
+	 */
+	public function searchBundle($bundleName)
+	{
+		$I = $this;
+		$I->Search(BundleManagerPage::$URL,$bundleName);
+	}
+
+	/**
+	 * @param $bundleName
+	 * @throws \Exception
+	 */
+	public function deleteBundle($bundleName)
+	{
+		$I = $this;
+		$I->amOnPage(BundleManagerPage::$URL);
+		$I->waitForText(BundleManagerPage::$bundleTitle, 30);
+		$I->Search(BundleManagerPage::$URL,$bundleName);
+		$I->see($bundleName, BundleManagerPage::$tableResult);
+		$I->click(BundleManagerPage::$checkAll);
+		$I->click(BundleManagerPage::$buttonDelete);
+		$I->waitForText(BundleManagerPage::$messageDeleteProductSuccess, 60, BundleManagerPage::$message);
+		$I->dontSee($bundleName);
+	}
+
+}
