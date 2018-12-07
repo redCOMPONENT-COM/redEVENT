8d0e3ef7c9475fd6550e37b0ce54c7e41e2e923b
VanTrieuThanh96 <thanhthanhvan96qn@gmail.com>
2018-12-07 13:24:03 +0700

    move redCORE to releases

--- tests/acceptance/install/02-InstallExtensionCest.php
+++ tests/acceptance/install/02-InstallExtensionCest.php
@@ -16,7 +16,7 @@ class InstallExtensionCest
 		$i->amOnPage('/administrator/index.php?option=com_installer');
 		$i->waitForText('Extensions: Install', 60, ['css' => 'H1']);
 
-        $pathredCORE = $i->getConfiguration('package url' . 'extension/redCORE/tests/releases/redCORE.zip');
+        $pathredCORE = $i->getConfiguration('package url' . '/redCORE.zip');
         $i->installExtensionFromFolder($pathredCORE);
 
         $i->click(['link' => 'Install from Folder']);
@@ -25,7 +25,7 @@ class InstallExtensionCest
 		$path = $i->getConfiguration('extension folder') . 'tests/extension/redFORM';
 		$i->installExtensionFromFolder($path);
 
-		$pathEvent = $i->getConfiguration('packages url' . 'releases/redevent.zip');
+		$pathEvent = $i->getConfiguration('packages url' . '/redevent.zip');
 		$i->installExtensionFromUrl($pathEvent);
 	}
 }
