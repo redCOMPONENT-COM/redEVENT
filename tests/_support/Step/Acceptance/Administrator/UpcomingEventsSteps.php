84ac9c2be5c7974b78843339f4fe107a1ab77f5e
unknown <trang.nguyen@redweb.dk>
2019-04-10 11:53:18 +0700

    update code

--- tests/_support/Step/Acceptance/Administrator/UpcomingEventsSteps.php
+++ tests/_support/Step/Acceptance/Administrator/UpcomingEventsSteps.php
@@ -101,4 +101,22 @@ class UpcomingEventsSteps extends AdminRedevent
 		$I->waitForText($venues,30,FrontendJoomlaManagerPage::$whereEvent);
 		$I->waitForText($category,30,FrontendJoomlaManagerPage::$categoryEvent);
 	}
+
+
+    public  function checkEventOfCategoryVenue ($menuitem, $sessionname,$eventName,$venues,$category, $sessionname1,$eventName1)
+    {
+        $I = $this;
+        $I->amOnPage(FrontendJoomlaManagerPage::$URL);
+        $I->checkForPhpNoticesOrWarningsOrExceptions();
+        $I->waitForText(FrontendJoomlaManagerPage::$title,30,AbstractPage::$H1);
+        $I->waitForText($menuitem,30);
+        $I->click($menuitem);
+        $I->waitForElement(FrontendJoomlaManagerPage::$eventList,30);
+        $I->waitForText($eventName,30);
+        $I->dontSee();
+        $I->click(FrontendJoomlaManagerPage::returnLink($eventName,$sessionname));
+        $I->waitForText(FrontendJoomlaManagerPage::returnLink($eventName,$sessionname),30,FrontendJoomlaManagerPage:: $titleEvent);
+        $I->waitForText($venues,30,FrontendJoomlaManagerPage::$whereEvent);
+        $I->waitForText($category,30,FrontendJoomlaManagerPage::$categoryEvent);
+    }
 }
