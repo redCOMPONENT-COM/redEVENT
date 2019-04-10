2b93a7350c0051975998bb7f0c650322853cbb07
unknown <trang.nguyen@redweb.dk>
2019-04-10 11:40:13 +0700

    add waitForElementVisible for select

--- tests/_support/Step/Acceptance/Administrator/SessionManagerSteps.php
+++ tests/_support/Step/Acceptance/Administrator/SessionManagerSteps.php
@@ -213,7 +213,7 @@ class SessionManagerSteps extends AdminRedevent
 		$I->waitForText(SessionManagerPage::$sessionTitle, 30);
 		$I->waitForElementVisible(SessionManagerPage::$searchTools,30);
 		$I->click(SessionManagerPage::$searchTools);
-		$I->waitForElement(SessionManagerPage::$filterPublished,30);
+		$I->waitForElementVisible(SessionManagerPage::$filterPublished,30);
 		$I->selectOptionInChosenById(SessionManagerPage::$filterPublishedID,"All");
 		$I->fillField(AbstractPage::$fieldSearch, $nameSession);
 		$I->click(AbstractPage::$buttonSearch);
