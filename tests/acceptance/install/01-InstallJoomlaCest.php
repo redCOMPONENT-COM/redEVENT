461896970c4675277b93eab13ff85bb055f98b82
VanTrieuThanh <thanh@redweb.dk>
2020-01-14 16:18:55 +0700

    refactor code"

--- tests/acceptance/install/01-InstallJoomlaCest.php
+++ tests/acceptance/install/01-InstallJoomlaCest.php
@@ -16,4 +16,25 @@ class InstallJoomlaCest
 		$I->disableStatistics();
 		$I->setErrorReportingToDevelopment();
 	}
+
+    public function install(\AcceptanceTester $i)
+    {
+        $i->wantToTest('redEVENT installation in Joomla 3');
+        $i->doAdministratorLogin();
+
+        $i->amOnPage('/administrator/index.php?option=com_installer');
+        $i->waitForText('Extensions: Install', 60, ['css' => 'H1']);
+
+        $pathredCORE = $i->getConfiguration('packages url').'redCORE.zip';
+        $i->installExtensionFromUrl($pathredCORE);
+
+        $i->click(['link' => 'Install from Folder']);
+        $i->comment('I enter the path');
+
+        $path = $i->getConfiguration('extension folder') . 'tests/extension/redFORM';
+        $i->installExtensionFromFolder($path);
+
+        $pathEvent = $i->getConfiguration('packages url').'redevent.zip';
+        $i->installExtensionFromUrl($pathEvent);
+    }
 }
