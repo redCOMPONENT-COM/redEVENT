1b2e8317c47ff034cd4aa11d820b71cd4f9eaab8
Oscar Iboy <oscar@redweb.dk>
2020-08-09 18:48:27 -0600

    REDEVENT-312 Codeception - Categories Layout

--- tests/_support/Step/Acceptance/Administrator/UpcomingEventsSteps.php
+++ tests/_support/Step/Acceptance/Administrator/UpcomingEventsSteps.php
@@ -1,13 +1,12 @@
 <?php
-
 /**
  * @package     redEVENT
  * @subpackage  Steps
  * @copyright   Copyright (C) 2008 - 2019 redCOMPONENT.com. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
-namespace Step\Acceptance\Administrator;
 
+namespace Step\Acceptance\Administrator;
 
 use Page\Acceptance\Administrator\AbstractPage;
 use Page\Acceptance\Administrator\FrontendJoomlaManagerPage;
@@ -17,117 +16,121 @@ class UpcomingEventsSteps extends AdminRedevent
 {
 	/**
 	 * @param $menuitem
-	 * @param $sessionname
+	 * @param $sessionName
 	 * @param $eventName
 	 * @throws \Exception
 	 */
-	public  function  checkEventUpcoming($menuitem, $sessionname,$eventName,$venues)
+	public  function  checkEventUpcoming($menuitem, $sessionName, $eventName)
 	{
 		$I = $this;
 		$I->doFrontEndLogin("admin","admin");
 		$I->amOnPage(FrontendJoomlaManagerPage::$URL);
 		$I->checkForPhpNoticesOrWarningsOrExceptions();
-		$I->waitForText(FrontendJoomlaManagerPage::$title,30,AbstractPage::$H1);
-		$I->waitForText($menuitem,30);
+		$I->waitForText(FrontendJoomlaManagerPage::$title, 30, AbstractPage::$H1);
+		$I->waitForText($menuitem, 30);
 		$I->click($menuitem);
 		$I->see($eventName);
-		$I->see($sessionname);
+		$I->see($sessionName);
 	}
 
 	/**
 	 * @param $menuItem
-	 * @param $sessioName
+	 * @param $sessionName
 	 * @param $eventName
 	 * @param $venues
 	 * @throws \Exception
 	 */
-	public  function  checkViewEventWithCalendar($menuItem, $sessioName,$eventName,$venues)
+	public  function  checkViewEventWithCalendar($menuItem, $sessionName, $eventName, $venues)
 	{
 		$I = $this;
 		$I->amOnPage(FrontendJoomlaManagerPage::$URL);
 		$I->checkForPhpNoticesOrWarningsOrExceptions();
-		$I->waitForText(FrontendJoomlaManagerPage::$title,30,AbstractPage::$H1);
-		$I->waitForText($menuItem,30);
+		$I->waitForText(FrontendJoomlaManagerPage::$title, 30, AbstractPage::$H1);
+		$I->waitForText($menuItem, 30);
 		$I->click($menuItem);
-		$I->waitForElement(FrontendJoomlaManagerPage::$calendar,30);
-		$I->waitForText($eventName,30,FrontendJoomlaManagerPage:: $calendarToday);
+		$I->waitForElement(FrontendJoomlaManagerPage::$calendar, 30);
+		$I->waitForText($eventName, 30, FrontendJoomlaManagerPage::$calendarToday);
 		$I->click($eventName);
-		$I->waitForText($sessioName,30);
+		$I->waitForText($sessionName,30);
 		$I->waitForText($venues,30);
 	}
 
 	/**
 	 * @param $menuitem
-	 * @param $sessionname
-	 * @param $eventName
-	 * @throws \Exception
-	 */
-	public  function  checkEventOnTable($menuitem, $sessionname,$eventName,$venues,$category)
-	{
-		$I = $this;
-		$I->doFrontEndLogin("admin","admin");
-		$I->amOnPage(FrontendJoomlaManagerPage::$URL);
-		$I->checkForPhpNoticesOrWarningsOrExceptions();
-		$I->waitForText(FrontendJoomlaManagerPage::$title,30,AbstractPage::$H1);
-		$I->waitForText($menuitem,30);
-		$I->click($menuitem);
-		$I->waitForElement(FrontendJoomlaManagerPage::$tableCategoryEvent,30);
-		$I->waitForText($eventName,30);
-		$I->click(FrontendJoomlaManagerPage::returnLink($eventName,$sessionname));
-		$I->waitForText(FrontendJoomlaManagerPage::returnLink($eventName,$sessionname),30,FrontendJoomlaManagerPage:: $titleEvent);
-		$I->waitForText($venues,30,FrontendJoomlaManagerPage::$whereEvent);
-		$I->waitForText($category,30,FrontendJoomlaManagerPage::$categoryEvent);
-	}
-
-	/**
-	 * @param $menuitem
-	 * @param $sessionname
-	 * @param $eventName
-	 * @throws \Exception
-	 */
-	public  function  checkEventArchive($menuitem, $sessionname,$eventName,$venues,$category)
-	{
-		$I = $this;
-		$I->doFrontEndLogin("admin","admin");
-		$I->amOnPage(FrontendJoomlaManagerPage::$URL);
-		$I->checkForPhpNoticesOrWarningsOrExceptions();
-		$I->waitForText(FrontendJoomlaManagerPage::$title,30,AbstractPage::$H1);
-		$I->waitForText($menuitem,30);
-		$I->click($menuitem);
-		$I->waitForElement(FrontendJoomlaManagerPage::$eventList,30);
-		$I->waitForText($eventName,30);
-		$I->click(FrontendJoomlaManagerPage::returnLink($eventName,$sessionname));
-		$I->waitForText(FrontendJoomlaManagerPage::returnLink($eventName,$sessionname),30,FrontendJoomlaManagerPage:: $titleEvent);
-		$I->waitForText($venues,30,FrontendJoomlaManagerPage::$whereEvent);
-		$I->waitForText($category,30,FrontendJoomlaManagerPage::$categoryEvent);
-	}
-
-	/**
-	 * @param $menuitem
-	 * @param $sessionname
+	 * @param $sessionName
 	 * @param $eventName
 	 * @param $venues
 	 * @param $category
 	 * @throws \Exception
 	 */
-	public function searchEventOnFrontEnd($menuitem, $sessionname,$eventName,$venues,$category)
+	public  function  checkEventOnTable($menuitem, $sessionName, $eventName, $venues, $category)
 	{
 		$I = $this;
 		$I->doFrontEndLogin("admin","admin");
 		$I->amOnPage(FrontendJoomlaManagerPage::$URL);
 		$I->checkForPhpNoticesOrWarningsOrExceptions();
-		$I->waitForText(FrontendJoomlaManagerPage::$title,30,AbstractPage::$H1);
-		$I->waitForText($menuitem,30);
+		$I->waitForText(FrontendJoomlaManagerPage::$title, 30, AbstractPage::$H1);
+		$I->waitForText($menuitem, 30);
 		$I->click($menuitem);
-		$I->waitForText(FrontendJoomlaManagerPage::$titleSearchEvent,30);
-		$I->waitForElement(FrontendJoomlaManagerPage::$tableSearch,30);
-		$I->waitForElement(FrontendJoomlaManagerPage::$fieldSearchFrontEnd,30);
-		$I->fillField(FrontendJoomlaManagerPage::$fieldSearchFrontEnd,$eventName);
+		$I->waitForElement(FrontendJoomlaManagerPage::$tableCategoryEvent, 30);
+		$I->waitForText($eventName, 30);
+		$I->click(FrontendJoomlaManagerPage::returnLink($eventName, $sessionName));
+		$I->waitForText(FrontendJoomlaManagerPage::returnLink($eventName, $sessionName), 30, FrontendJoomlaManagerPage::$titleEvent);
+		$I->waitForText($venues, 30, FrontendJoomlaManagerPage::$whereEvent);
+		$I->waitForText($category, 30, FrontendJoomlaManagerPage::$categoryEvent);
+	}
+
+	/**
+	 * @param $menuitem
+	 * @param $sessionName
+	 * @param $eventName
+	 * @param $venues
+	 * @param $category
+	 * @throws \Exception
+	 */
+	public  function  checkEventArchive($menuitem, $sessionName, $eventName, $venues, $category)
+	{
+		$I = $this;
+		$I->doFrontEndLogin("admin","admin");
+		$I->amOnPage(FrontendJoomlaManagerPage::$URL);
+		$I->checkForPhpNoticesOrWarningsOrExceptions();
+		$I->waitForText(FrontendJoomlaManagerPage::$title, 30, AbstractPage::$H1);
+		$I->waitForText($menuitem, 30);
+		$I->click($menuitem);
+		$I->waitForElement(FrontendJoomlaManagerPage::$eventList, 30);
+		$I->waitForText($eventName, 30);
+		$I->click(FrontendJoomlaManagerPage::returnLink($eventName, $sessionName));
+		$I->waitForText(FrontendJoomlaManagerPage::returnLink($eventName, $sessionName),30, FrontendJoomlaManagerPage::$titleEvent);
+		$I->waitForText($venues, 30, FrontendJoomlaManagerPage::$whereEvent);
+		$I->waitForText($category, 30, FrontendJoomlaManagerPage::$categoryEvent);
+	}
+
+	/**
+	 * @param $menuitem
+	 * @param $sessionName
+	 * @param $eventName
+	 * @param $venues
+	 * @param $category
+	 * @throws \Exception
+	 */
+	public function searchEventOnFrontEnd($menuitem, $sessionName,$eventName,$venues,$category)
+	{
+		$I = $this;
+		$I->doFrontEndLogin("admin","admin");
+		$I->amOnPage(FrontendJoomlaManagerPage::$URL);
+		$I->checkForPhpNoticesOrWarningsOrExceptions();
+		$I->waitForText(FrontendJoomlaManagerPage::$title, 30, AbstractPage::$H1);
+		$I->waitForText($menuitem, 30);
+		$I->click($menuitem);
+		$I->waitForText(FrontendJoomlaManagerPage::$titleSearchEvent, 30);
+		$I->waitForElement(FrontendJoomlaManagerPage::$tableSearch, 30);
+		$I->waitForElement(FrontendJoomlaManagerPage::$fieldSearchFrontEnd, 30);
+		$I->fillField(FrontendJoomlaManagerPage::$fieldSearchFrontEnd, $eventName);
 		$I->click(FrontendJoomlaManagerPage::$buttonSearchFrontEnd);
-		$I->waitForText(FrontendJoomlaManagerPage::returnLink($eventName,$sessionname),30,FrontendJoomlaManagerPage::$titleOnTable);
-		$I->click(FrontendJoomlaManagerPage::returnLink($eventName,$sessionname));
-		$I->waitForText(FrontendJoomlaManagerPage::returnLink($eventName,$sessionname),30,FrontendJoomlaManagerPage:: $titleEvent);
-		$I->waitForText($venues,30,FrontendJoomlaManagerPage::$whereEvent);
-		$I->waitForText($category,30,FrontendJoomlaManagerPage::$categoryEvent);
+		$I->waitForText(FrontendJoomlaManagerPage::returnLink($eventName, $sessionName), 30, FrontendJoomlaManagerPage::$titleOnTable);
+		$I->click(FrontendJoomlaManagerPage::returnLink($eventName, $sessionName));
+		$I->waitForText(FrontendJoomlaManagerPage::returnLink($eventName, $sessionName), 30, FrontendJoomlaManagerPage::$titleEvent);
+		$I->waitForText($venues, 30, FrontendJoomlaManagerPage::$whereEvent);
+		$I->waitForText($category, 30, FrontendJoomlaManagerPage::$categoryEvent);
 	}
 }
--- tests/acceptance/integration/CategoriesLayout/CategoriesLayoutCest.php
+++ tests/acceptance/integration/CategoriesLayout/CategoriesLayoutCest.php
@@ -2,7 +2,7 @@
 /**
  * @package     redEVENT
  * @subpackage  Cest
- * @copyright   Copyright (C) 2008 - 2019 redCOMPONENT.com. All rights reserved.
+ * @copyright   Copyright (C) 2008 - 2020 redCOMPONENT.com. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
@@ -10,86 +10,92 @@ use Faker\Factory;
 use Page\Acceptance\Administrator\AbstractPage;
 use Step\Acceptance\Administrator\EventManagerSteps;
 use Step\Acceptance\Administrator\SessionManagerSteps;
-use Step\Acceptance\Administrator\UpcomingEventsSteps;
 use Step\Acceptance\Administrator\VanueManagerSteps;
+use Step\Acceptance\Administrator\CategoriesLayoutSteps;
 use Step\Acceptance\JoomlaManagerSteps;
 
-class CategoryEventsTableCest
+/**
+ * Class CategoriesLayoutCest
+ * @since 3.2.9
+ */
+class CategoriesLayoutCest
 {
 	/**
-	 * @var   Generator
-	 * @since 1.0.0
+	 * @var Generator
+	 * @since 3.2.9
 	 */
 	protected $faker;
 
 	/**
-	 * @var   string
-	 * @since 1.0.0
+	 * @var string
+	 * @since 3.2.9
 	 */
-	protected $categoryVanueName;
+	protected $categoryVenueName;
 
 	/**
-	 * @var   string
-	 * @since 1.0.0
+	 * @var string
+	 * @since 3.2.9
 	 */
-	protected $VanueName;
+	protected $venueName;
 
 	/**
-	 * @var   string
-	 * @since 1.0.0
+	 * @var string
+	 * @since 3.2.9
 	 */
 	protected $categoryName;
 
 	/**
-	 * @var   string
-	 * @since 1.0.0
+	 * @var string
+	 * @since 3.2.9
 	 */
 	protected $eventName;
 
 	/**
-	 * @var   string
-	 * @since 1.0.0
+	 * @var string
+	 * @since 3.2.9
 	 */
 	protected $templateName;
 
 	/**
-	 * @var   string
-	 * @since 1.0.0
+	 * @var string
+	 * @since 3.2.9
 	 */
-	protected $SessionName;
+	protected $sessionName;
 
 	/**
 	 * @var string
+	 * @since 3.2.9
 	 */
 	protected $menuItem;
 
 	/**
 	 * @var string
+	 * @since 3.2.9
 	 */
 	protected  $menuCategory;
 
 	/**
-	 * VenueCategoryManagerCest constructor.
-	 *
-	 * @since 1.0.0
+	 * CategoriesLayoutCest constructor.
+	 * @since 3.2.9
 	 */
 	public function __construct()
 	{
 		$this->faker             = Factory::create();
-		$this->categoryVanueName = $this->faker->bothify("Category Vanue Name ##??");
-		$this->VanueName         = $this->faker->bothify("Vanue Name ##??");
+		$this->categoryVenueName = $this->faker->bothify("Category Venue Name ##??");
+		$this->venueName         = $this->faker->bothify("Venue Name ##??");
 		$this->categoryName      = $this->faker->bothify("Category Name ##??");
 		$this->eventName         = $this->faker->bothify("Event Name ##??");
 		$this->templateName      = 'default template';
-		$this->SessionName       = $this->faker->bothify("Session Name ##??");
+		$this->sessionName       = $this->faker->bothify("Session Name ##??");
 
-		$this->menuItem          = 'Category events table layout';
+		$this->menuItem          = 'Categories Layout';
 		$this->menuCategory      = 'redEVENT - Component';
 	}
 
 	/**
 	 * @param VanueManagerSteps $i
 	 * @throws Exception
+	 * @since 3.2.9
 	 */
 	public function _before(VanueManagerSteps $i)
 	{
@@ -99,67 +105,71 @@ class CategoryEventsTableCest
 	/**
 	 * @param VanueManagerSteps $I
 	 * @throws Exception
+	 * @since 3.2.9
 	 */
 	public function addVenue(VanueManagerSteps $I)
 	{
 		$I->wantToTest('Add a venue in redEVENT');
-
-		$I->createVenueNew($this->VanueName, $this->categoryVanueName);
+		$I->createVenueNew($this->venueName,$this->categoryVenueName);
 		$I->waitForText(AbstractPage::$messageSaveSuccess, 30, AbstractPage::$message);
 	}
 
 	/**
 	 * @param EventManagerSteps $I
 	 * @throws Exception
+	 * @since 3.2.9
 	 */
 	public function addEvent(EventManagerSteps $I)
 	{
 		$I->wantToTest('Add an event in redEVENT with default template');
-
-		$I->createEventNew($this->eventName, $this->categoryName, $this->templateName);
+		$I->createEventNew($this->eventName,$this->categoryName, $this->templateName);
 		$I->waitForText(AbstractPage::$messageSaveSuccess, 30, AbstractPage::$message);
 	}
 
 	/**
 	 * @param SessionManagerSteps $I
 	 * @throws Exception
+	 * @since 3.2.9
 	 */
 	public function createSession(SessionManagerSteps $I)
 	{
 		$I->wantToTest('Add session in redEVENT');
-		$I->createSessionNew($this->eventName, $this->VanueName, $this->SessionName);
+		$I->createSessionNew($this->eventName,$this->venueName,$this->sessionName);
 		$I->waitForText(AbstractPage::$messageSaveSuccess, 30, AbstractPage::$message);
 	}
 
 	/**
 	 * @param JoomlaManagerSteps $I
 	 * @throws Exception
+	 * @since 3.2.9
 	 */
 	public function createMenuItem(JoomlaManagerSteps $I)
 	{
-		$I->wantTo("Create Menu item Featured events in front end");
-		$I->createNewMenuItemHaveCategory($this->menuItem, $this->menuCategory, $this->menuItem, $this->categoryName);
+		$I->wantTo("Create menu item featured events in frontend");
+		$I->createNewMenuItem($this->menuItem, $this->menuCategory, $this->menuItem);
 	}
 
 	/**
-	 * @param UpcomingEventsSteps $I
+	 * @param CategoriesLayoutSteps $I
 	 * @throws Exception
+	 * @since 3.2.9
 	 */
-	public function CheckFrontEndWithFeaturedEvents(UpcomingEventsSteps $I)
+	public function checkFrontEndCategoriesLayout(CategoriesLayoutSteps $I)
 	{
-		$I->wantToTest('Check featured events on front-end');
-		$I->checkEventOnTable($this->menuItem, $this->SessionName, $this->eventName, $this->VanueName, $this->categoryName);
+		$I->wantToTest('Check categories layout on front-end');
+		$I->checkCategoriesLayout($this->menuItem, $this->eventName, $this->categoryName);
 	}
 
-	/***
+	/**
 	 * @param SessionManagerSteps $I
 	 * @param $scenario
 	 * @throws Exception
+	 * @since 3.2.9
 	 */
 	public function deleteAll(SessionManagerSteps $I, $scenario)
 	{
 		$I->wantToTest('Delete session');
-		$I ->deleteSession($this->SessionName);
+		$I ->deleteSession($this->sessionName);
 		$I = new EventManagerSteps($scenario);
 		$I->wantToTest('Delete events');
 		$I->deleteEvent($this->eventName);
@@ -167,6 +177,6 @@ class CategoryEventsTableCest
 		$I->deleteCategory($this->categoryName);
 		$I = new VanueManagerSteps($scenario);
 		$I->wantToTest('Delete Venue');
-		$I->deleteVenue($this->categoryVanueName, $this->VanueName);
+		$I->deleteVenue($this->categoryVenueName, $this->venueName);
 	}
 }
