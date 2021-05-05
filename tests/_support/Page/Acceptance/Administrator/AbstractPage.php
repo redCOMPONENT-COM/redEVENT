07d696d31f025bd41f0eaf8d690e4d7845499de3
unknown <trang.nguyen@redweb.dk>
2021-05-05 10:29:39 +0700

    change xpath, fix style

--- tests/_support/Page/Acceptance/Administrator/AbstractPage.php
+++ tests/_support/Page/Acceptance/Administrator/AbstractPage.php
@@ -16,112 +16,112 @@ class AbstractPage
 	 * @var array
 	 * @since 1.0.0
 	 */
-	public static $H1                         = ['css' => 'H1'];
+	public static $H1 = '//h1';
 
 	/**
 	 * Locator for label
 	 * @var array
 	 * @since 1.0.0
 	 */
-	public static $label                      = ['css' => 'label'];
+	public static $label = '//label';
 
 	/**
 	 * Locator for field name
 	 * @var array
 	 * @since 1.0.0
 	 */
-	public static $fieldName                   = '#jform_name';
+	public static $fieldName = '#jform_name';
 
 	/**
 	 * Locator for field description
 	 * @var array
 	 * @since 1.0.0
 	 */
-	public static $fieldDescription            = 'jform_description';
+	public static $fieldDescription = 'jform_description';
 
 	/**
 	 * Locator for message
 	 * @var string
 	 */
-	public static $message                     = '#system-message-container';
+	public static $message = '#system-message-container';
 
 	/**
 	 * Button new
 	 * @var string
 	 */
-	public static $buttonNew                   = 'New';
+	public static $buttonNew = 'New';
 
 	/**
 	 * Button edit
 	 * @var string
 	 */
-	public static $buttonEdit                  = 'Edit';
+	public static $buttonEdit = 'Edit';
 
 	/**
 	 * Button Import/Export
 	 * @var string
 	 */
-	public static $buttonImportExport          = 'Import/Export';
+	public static $buttonImportExport = 'Import/Export';
 
 	/**
 	 * Button Publish
 	 * @var string
 	 */
-	public static $buttonPublish               = 'Publish';
+	public static $buttonPublish = 'Publish';
 
 	/**
 	 * Button unpublish
 	 * @var string
 	 */
-	public static $buttonUnpublish             = 'Unpublish';
+	public static $buttonUnpublish = 'Unpublish';
 
 	/**
 	 * Button Delete
 	 * @var string
 	 */
-	public static $buttonDelete                = 'Delete';
+	public static $buttonDelete = 'Delete';
 
 	/**
 	 * Button Save
 	 * @var string
 	 */
-	public static $buttonSave                  = 'Save';
+	public static $buttonSave = 'Save';
 
 	/**
 	 * Button Save & Close
 	 * @var string
 	 */
-	public static $buttonSaveClose             = 'Save & Close';
+	public static $buttonSaveClose = 'Save & Close';
 
 	/**
 	 * Button Save & New
 	 * @var string
 	 */
-	public static $buttonSaveNew               = 'Save & New';
+	public static $buttonSaveNew = 'Save & New';
 
 	/**
 	 * Button Save as Copy
 	 * @var string
 	 */
-	public static $buttonSaveCopy              = 'Save as Copy';
+	public static $buttonSaveCopy = 'Save as Copy';
 
 	/**
 	 * Button Cancel
 	 * @var string
 	 */
-	public static $buttonCancel                = 'Cancel';
+	public static $buttonCancel = 'Cancel';
 
 	/**
 	 * Button Clear
 	 * @var string
 	 */
-	public static $buttonClear                 = 'Clear';
+	public static $buttonClear = 'Clear';
 
 	/**
 	 * Message Save Success
 	 * @var string
 	 */
-	public static $messageSaveSuccess          = 'Item saved.';
+	public static $messageSaveSuccess = 'Item saved.';
 
 	/**
 	 * Message delete success
@@ -139,19 +139,19 @@ class AbstractPage
 	 * Button search
 	 * @var string
 	 */
-	public static $buttonSearch                = '//button[@type=\'submit\' and @data-original-title=\'Search\']';
+	public static $buttonSearch = '//button[@type=\'submit\' and @data-original-title=\'Search\']';
 
 	/**
 	 * Field search
 	 * @var string
 	 */
-	public static $fieldSearch                 = '#filter_search';
+	public static $fieldSearch = '#filter_search';
 
 	/**
 	 * Locator for  Name item
 	 * @var string
 	 */
-	public static $tableResult                = '//table[@id=\'table-items\']/tbody/tr[1]/td[5]';
+	public static $tableResult = "//table[@id='table-items']/tbody/tr[1]";
 
 	/**
 	 * @var string
